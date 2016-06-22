4860330862c3a2bc015979e37cdb5673acfa9381
mingo1214 <mingo1214@gmail.com>
2016-06-21 21:35:09 -0400

    The first build of HB_updateLogs

---
+++ UpdateLogs/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace updatelogs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(@"UPDATELOGS.md"))
+                {
+                    String line = await sr.ReadToEndAsync();
+                    textBox1.Text = line;
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = "There is no update logs available now!";
+            }
+        }
+    }
+}
