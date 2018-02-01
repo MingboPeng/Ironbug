87ef17e0f22bc4835be497b16f03407e2c221105
MingboPeng <mingo1214@gmail.com>
2018-01-31 22:57:53 -0500

    started Ironbug.HVAC

---
+++ src/Ironbug.HVAC.Test/ExtClassTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ironbug.HVAC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ironbug.HVAC.Test
+{
+    [TestClass()]
+    public class ExtClassTests
+    {
+        [TestMethod()]
+        public void CopyToTest()
+        {
+            string sFile = @"..\..\..\..\doc\osmFile\pkVAV_HW.osm";
+            string tFile = @"..\..\..\..\doc\osmFile\empty.osm";
+
+            string saveFile = @"..\..\..\..\doc\osmFile\empty_.osm";
+
+            var sModel = OpenStudio.Model.load(new OpenStudio.Path(sFile)).get();
+            var tModel = OpenStudio.Model.load(new OpenStudio.Path(tFile)).get();
+
+            var loops = sModel.getPlantLoops();
+            if (loops.Any())
+            {
+                foreach (var loop in loops)
+                {
+                    loop.CloneTo(tModel);
+                }
+            }
+
+            var success = tModel.Save(saveFile);
+
+            Assert.AreEqual(success, true);
+        }
+    }
+}
---
+++ src/Ironbug.HVAC/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenStudio;
+
+namespace Ironbug.HVAC
+{
+    public static class Extensions
+    {
+        public static void CloneTo (this PlantLoop fromPlant, Model model)
+        {
+            var plantLoop = new PlantLoop(model);
+            plantLoop.setName("_" + fromPlant.nameString());
+
+            //copy plant loop settings
+
+
+            //copy sizingPlant settings
+            var sizing = fromPlant.sizingPlant().CloneTo(model, plantLoop);
+            
+        }
+
+        public static SizingPlant CloneTo(this SizingPlant szPlant, Model model, PlantLoop plantLoop)
+        {
+            var s = new SizingPlant(model, plantLoop);
+            
+            s.setLoopType(szPlant.loopType());
+            s.setLoopDesignTemperatureDifference(szPlant.loopDesignTemperatureDifference());
+            s.setDesignLoopExitTemperature(szPlant.designLoopExitTemperature());
+
+            return s;
+            
+        }
+
+        public static bool Save(this Model model, string filePath)
+        {
+            return model.save(new Path(filePath), true);
+
+        }
+    }
+}
