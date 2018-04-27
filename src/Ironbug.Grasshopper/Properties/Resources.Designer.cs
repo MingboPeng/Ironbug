c163740bada60760f3a9ba24797657503701ff62
Mingbo Peng <mingo1214@gmail.com>
2018-04-27 13:22:54 -0400

    added condenser water loop

--- src/Ironbug.Grasshopper/Properties/Resources.Designer.cs
+++ src/Ironbug.Grasshopper/Properties/Resources.Designer.cs
@@ -343,6 +343,16 @@ namespace Ironbug.Grasshopper.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap PlantLoopDW {
+            get {
+                object obj = ResourceManager.GetObject("PlantLoopDW", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap PlantLoopHW {
             get {
                 object obj = ResourceManager.GetObject("PlantLoopHW", resourceCulture);
