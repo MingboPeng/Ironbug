6c5d75107c4c405703acec9df2c61c32358625fa
Mingbo Peng <mingo1214@gmail.com>
2018-06-02 21:49:00 -0400

    added ZoneHVACFourPipeFanCoil

---
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_ZoneHVACFourPipeFanCoil.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Ironbug.HVAC;
+using Ironbug.HVAC.BaseClass;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_ZoneHVACFourPipeFanCoil : Ironbug_HVACComponentBase
+    {
+        
+        public Ironbug_ZoneHVACFourPipeFanCoil()
+          : base("Ironbug_ZoneHVACFourPipeFanCoil", "UnitVentCH",
+              "Description",
+              "Ironbug", "04:ZoneEquipments",
+              typeof(IB_ZoneHVACFourPipeFanCoil_DataFieldSet))
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("HeatingCoil", "coilH_", "Heating coil to provide heating source. Must be CoilHeatingWater.", GH_ParamAccess.item);
+            pManager[0].Optional = true;
+            pManager.AddGenericParameter("CoolingCoil", "coilC_", "Cooling coil to provide cooling source. Must be CoilHeatingWater.", GH_ParamAccess.item);
+            pManager[1].Optional = true;
+            pManager.AddGenericParameter("Fan", "fan_", "Can be FanOnOff, FanConstantVolume or FanVariableVolume.", GH_ParamAccess.item);
+            pManager[2].Optional = true;
+        }
+        
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("ZoneHVACUnitVentilator_CoolingHeating", "UnitVentCH", "Connect to zone's equipment", GH_ParamAccess.item);
+        }
+        
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var obj = new HVAC.IB_ZoneHVACFourPipeFanCoil();
+            obj.PuppetEventHandler += PuppetStateChanged;
+
+            var fan = (IB_Fan)null;
+            var coilH = (IB_CoilHeatingWater)null;
+            var coilC = (IB_CoilCoolingWater)null;
+
+            if (DA.GetData(0, ref coilH))
+            {
+                obj.SetHeatingCoil(coilH);
+            }
+
+            if (DA.GetData(1, ref coilC))
+            {
+                obj.SetCoolingCoil(coilC);
+            }
+
+            if (DA.GetData(2, ref fan))
+            {
+                obj.SetFan(fan);
+            }
+
+
+            this.SetObjParamsTo(obj);
+            DA.SetData(0, obj);
+        }
+        
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+        
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("A5704E42-164F-4BB1-8412-A44B66B6370D"); }
+        }
+    }
+}
---
+++ src/Ironbug.HVAC/ZoneEquipments/ZoneHVAC/IB_ZoneHVACFourPipeFanCoil.cs
@@ -0,0 +1,61 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC
+{
+    public class IB_ZoneHVACFourPipeFanCoil : IB_ZoneEquipment
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_ZoneHVACFourPipeFanCoil();
+
+        private static ZoneHVACFourPipeFanCoil InitMethod(Model model)
+            => new ZoneHVACFourPipeFanCoil(model, model.alwaysOnDiscreteSchedule(), new FanConstantVolume(model), new CoilCoolingWater(model), new CoilHeatingWater(model));
+
+        private IB_Child CoolingCoil => this.Children.GetChild<IB_CoilCoolingWater>();
+        private IB_Child HeatingCoil => this.Children.GetChild<IB_CoilHeatingWater>();
+        private IB_Child Fan => this.Children.GetChild<IB_Fan>();
+
+        public IB_ZoneHVACFourPipeFanCoil() : base(InitMethod(new Model()))
+        {
+            var hCoil = new IB_Child(new IB_CoilHeatingWater(), (obj) => this.SetHeatingCoil(obj as IB_CoilHeatingWater));
+            var cCoil = new IB_Child(new IB_CoilCoolingWater(), (obj) => this.SetCoolingCoil(obj as IB_CoilCoolingWater));
+            var fan = new IB_Child(new IB_FanConstantVolume(), (obj) => this.SetFan(obj as IB_Fan));
+            this.Children.Add(hCoil);
+            this.Children.Add(cCoil);
+            this.Children.Add(fan);
+        }
+
+        public void SetFan(IB_Fan Fan)
+        {
+            this.Fan.Set(Fan);
+        }
+
+        public void SetHeatingCoil(IB_CoilHeatingWater Coil)
+        {
+            this.HeatingCoil.Set(Coil);
+        }
+
+        public void SetCoolingCoil(IB_CoilCoolingWater Coil)
+        {
+            this.CoolingCoil.Set(Coil);
+        }
+
+        protected override ModelObject InitOpsObj(Model model)
+        {
+            var opsObj = base.OnInitOpsObj(InitMethod, model).to_ZoneHVACFourPipeFanCoil().get();
+            opsObj.setCoolingCoil((HVACComponent)this.CoolingCoil.To<IB_CoilCoolingWater>().ToOS(model));
+            opsObj.setHeatingCoil((HVACComponent)this.HeatingCoil.To<IB_CoilHeatingWater>().ToOS(model));
+            opsObj.setSupplyAirFan((HVACComponent)this.Fan.To<IB_Fan>().ToOS(model));
+            return opsObj;
+        }
+    }
+
+    public sealed class IB_ZoneHVACFourPipeFanCoil_DataFieldSet
+        : IB_DataFieldSet<IB_ZoneHVACFourPipeFanCoil_DataFieldSet, ZoneHVACFourPipeFanCoil>
+    {
+        private IB_ZoneHVACFourPipeFanCoil_DataFieldSet() {}
+        public IB_DataField CapacityControlMethod { get; }
+            = new IB_BasicDataField("CapacityControlMethod", "CapacityCtrl");
+        
+    }
+}
