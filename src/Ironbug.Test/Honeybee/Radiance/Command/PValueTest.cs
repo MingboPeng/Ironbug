ab4591dc0bc2b6e0f944ed678071a1f933719476
Mingbo Peng <mingo1214@gmail.com>
2018-12-10 04:55:15 -0500

    added Radiance PValue test

---
+++ src/Ironbug.Test/Honeybee/Radiance/Command/PValueTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ironbug.Honeybee.Radiance.Command;
+
+namespace Ironbug.Test.Honeybee.Radiance.Command
+{
+    [TestClass]
+    public class PValueTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void PValueTest_Exe()
+        {
+            Process cmd = new Process()
+            {
+
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C pvalue -o -d -h -b Room.HDR",
+                    WorkingDirectory = @"C:\Users\mingo\OneDrive\Desktop\",
+                    CreateNoWindow = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false
+                }
+
+            };
+
+            cmd.StartInfo.EnvironmentVariables["PATH"] += String.Format(";{0}", RadianceBaseCommand.normspace(Config.RadbinPath));
+            cmd.StartInfo.EnvironmentVariables["RAYPATH"] += String.Format(";{0}", RadianceBaseCommand.normspace(Config.RadlibPath));
+
+            cmd.Start();
+            //cmd.BeginOutputReadLine();
+            string outputs = cmd.StandardOutput.ReadToEnd();
+            string err = cmd.StandardError.ReadToEnd();
+
+            //Console.WriteLine(outputs);
+            
+            Console.WriteLine(err);
+
+            //TestContext.WriteLine(outputs);
+            //TestContext.WriteLine("Error: "+ err);
+
+            cmd.WaitForExit();
+
+            //while (!cmd.HasExited)
+            //{
+            //    int milliseconds = 25;
+            //    Thread.Sleep(milliseconds);
+            //}
+
+            cmd.Close();
+
+            var lines = outputs.Split(Environment.NewLine.ToCharArray());
+            Console.WriteLine("Total length: "+lines.Length);
+            Console.WriteLine(lines[0]);
+            Assert.IsTrue(lines.Length > 0);
+        }
+    }
+}
