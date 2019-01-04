d7b2379cc62e702b170a798b852f6665ab484714
Mingbo Peng <mingo1214@gmail.com>
2019-01-03 22:02:28 -0500

    Tested HVAC Template generator successfully.

--- src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_HVACTemplate.cs
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_HVACTemplate.cs
@@ -3,6 +3,7 @@ using System;
 using Grasshopper.Kernel.Special;
 using GH = Grasshopper;
 using System.Drawing;
+using System.IO;
 
 namespace Ironbug.Grasshopper.Component
 {
@@ -33,34 +34,67 @@ namespace Ironbug.Grasshopper.Component
             DA.GetData(0, ref run);
             if (!run) return;
 
-            var ghBoiler = new Ironbug_BoilerHotWater();
-            ghBoiler.CreateAttributes();
-            var boilerP = new PointF(this.Attributes.Bounds.Left - 80, this.Attributes.Bounds.Y + 200);
-            ghBoiler.Attributes.Pivot = boilerP;
-            GH.Instances.ActiveCanvas.Document.AddObject(ghBoiler, false);
-            //ghBoiler.ExpireSolution(true);
+            //var ghBoiler = new Ironbug_BoilerHotWater();
+            //ghBoiler.CreateAttributes();
+            //var boilerP = new PointF(this.Attributes.Bounds.Left - 80, this.Attributes.Bounds.Y + 200);
+            //ghBoiler.Attributes.Pivot = boilerP;
+            //GH.Instances.ActiveCanvas.Document.AddObject(ghBoiler, false);
+            ////ghBoiler.ExpireSolution(true);
 
-            var pLoop = new Ironbug_PlantBranches();
-            pLoop.CreateAttributes();
-            pLoop.Attributes.Pivot = new PointF(boilerP.X + 150, boilerP.Y);
-            GH.Instances.ActiveCanvas.Document.AddObject(pLoop, false);
+            //var pLoop = new Ironbug_PlantBranches();
+            //pLoop.CreateAttributes();
+            //pLoop.Attributes.Pivot = new PointF(boilerP.X + 150, boilerP.Y);
+            //GH.Instances.ActiveCanvas.Document.AddObject(pLoop, false);
 
-            pLoop.Params.Input[0].AddSource(ghBoiler.Params.Output[0]);
+            //pLoop.Params.Input[0].AddSource(ghBoiler.Params.Output[0]);
 
-            //create a group
-            var GhGroup = new GH_Group();
-            GhGroup.CreateAttributes();
-            GhGroup.NickName = "Extracted Coordinates";
-            GhGroup.Colour = Color.White;
-            GhGroup.AddObject(ghBoiler.InstanceGuid);
-            GhGroup.AddObject(pLoop.InstanceGuid);
-            GH.Instances.ActiveCanvas.Document.AddObject(GhGroup, false);
-            //GhGroup.ExpireSolution(true);
-
-
+            ////create a group
+            //var GhGroup = new GH_Group();
+            //GhGroup.CreateAttributes();
+            //GhGroup.NickName = "Extracted Coordinates";
+            //GhGroup.Colour = Color.White;
+            //GhGroup.AddObject(ghBoiler.InstanceGuid);
+            //GhGroup.AddObject(pLoop.InstanceGuid);
+            //GH.Instances.ActiveCanvas.Document.AddObject(GhGroup, false);
+            ////GhGroup.ExpireSolution(true);
 
 
             //GH.Instances.ActiveCanvas.Document.NewSolution(false);
+
+            CreateTemplateFromXMLString(@"C:\Ironbug\HVACTemplates\Default\Test.txt", ref run);
+        }
+
+        private static void CreateTemplateFromXMLString(string FilePath, ref bool Run)
+        {
+            var canvasCurrent = GH.Instances.ActiveCanvas;
+            var f = canvasCurrent.Focused;
+            var isFileExist = File.Exists(FilePath);
+
+            if (Run && f && isFileExist)
+            {
+                var tempStr = File.ReadAllText(FilePath);
+                System.Windows.Forms.Clipboard.Clear();
+                System.Windows.Forms.Clipboard.SetText(tempStr);
+
+                var io = new GH_DocumentIO();
+                var success = io.Paste(GH_ClipboardType.System);
+
+                if (!success)
+                {
+                    System.Windows.Forms.MessageBox.Show("Failed");
+                    return;
+                }
+                var docTemp = io.Document;
+
+                docTemp.SelectAll();
+                docTemp.MutateAllIds();
+                docTemp.ExpireSolution();
+                var objects = docTemp.Objects;
+
+                var docCurrent = canvasCurrent.Document;
+                docCurrent.DeselectAll();
+                docCurrent.MergeDocument(docTemp);
+            }
         }
     }
 }
