9146c7bd1d966b07fc0bd89d7686f8fd6f041fb2
Mingbo Peng <mingo1214@gmail.com>
2019-01-05 23:59:41 -0500

    fixed typo SpSZReheat

--- src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_SetpointManagerSingleZoneReheat.cs
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_SetpointManagerSingleZoneReheat.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using Grasshopper.Kernel;
+using System;
 using System.Collections.Generic;
-
-using Grasshopper.Kernel;
-using Rhino.Geometry;
 
 namespace Ironbug.Grasshopper.Component.Ironbug
 {
@@ -14,8 +12,9 @@ namespace Ironbug.Grasshopper.Component.Ironbug
               "Ironbug", "05:SetpointManager")
         {
         }
+
         public override GH_Exposure Exposure => GH_Exposure.primary;
-        
+
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddGenericParameter("ControlZone", "_zone", "Control Zone", GH_ParamAccess.list);
@@ -24,14 +23,13 @@ namespace Ironbug.Grasshopper.Component.Ironbug
             pManager.AddNumberParameter("maxTemperature", "_maxT_", "MaximumSupplyAirTemperature", GH_ParamAccess.item, 50);
             pManager[1].Optional = true;
             pManager[2].Optional = true;
-
         }
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddGenericParameter("SetpointManagerSingleZoneReheat", "SpWarmest", "TODO:...", GH_ParamAccess.item);
+            pManager.AddGenericParameter("SetpointManagerSingleZoneReheat", "SpSZReheat", "TODO:...", GH_ParamAccess.item);
         }
-        
+
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             var zones = new List<HVAC.BaseClass.IB_ThermalZone>();
@@ -39,33 +37,22 @@ namespace Ironbug.Grasshopper.Component.Ironbug
             var zone = (HVAC.BaseClass.IB_ThermalZone)null;
             if (zones.Count == 0) return;
 
-
             if (zones.Count > 1) this.AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "This is the setpointManager for single zone, you have more than one zone as input. So it takes the first zone as the control zone.");
             zone = zones[0];
             var obj = new HVAC.IB_SetpointManagerSingleZoneReheat(zone);
             double minT = 10;
             double maxT = 50;
-            
-            
+
             var fieldSet = HVAC.IB_SetpointManagerSingleZoneReheat_DataFieldSet.Value;
 
             if (DA.GetData(1, ref minT)) obj.SetFieldValue(fieldSet.MinimumSupplyAirTemperature, minT);
             if (DA.GetData(2, ref maxT)) obj.SetFieldValue(fieldSet.MaximumSupplyAirTemperature, maxT);
-            
+
             DA.SetData(0, obj);
         }
-        
-        protected override System.Drawing.Bitmap Icon
-        {
-            get
-            {
-                return Properties.Resources.SetPointSZReheat;
-            }
-        }
-        
-        public override Guid ComponentGuid
-        {
-            get { return new Guid("DE6F5B3A-0F30-4205-B872-22698641AB32"); }
-        }
+
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.SetPointSZReheat;
+
+        public override Guid ComponentGuid => new Guid("DE6F5B3A-0F30-4205-B872-22698641AB32");
     }
 }
