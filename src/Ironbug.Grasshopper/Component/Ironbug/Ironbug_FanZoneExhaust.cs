25293975f16634373749a3486ad373a7023bc178
Mingbo Peng <mingo1214@gmail.com>
2019-01-28 21:11:44 -0500

    added FanZoneExhaust

---
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_FanZoneExhaust.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Ironbug.HVAC;
+using Ironbug.HVAC.BaseClass;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_FanZoneExhaust : Ironbug_HVACComponentBase
+    {
+
+        protected override System.Drawing.Bitmap Icon => null;
+
+        public override Guid ComponentGuid => new Guid("FFCB9844-A425-4D06-A1B3-9C2ABBB5BF97");
+
+        public override GH_Exposure Exposure => GH_Exposure.tertiary;
+        
+        public Ironbug_FanZoneExhaust()
+          : base("Ironbug_FanZoneExhaust", "ExFan",
+              "Description",
+              "Ironbug", "04:ZoneEquipments",
+              typeof(IB_FanZoneExhaust_DataFieldSet))
+        {
+        }
+
+        
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+        }
+        
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("FanZoneExhaust", "ExFan", "Connect to zone's equipment", GH_ParamAccess.item);
+        }
+        
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var obj = new HVAC.IB_FanZoneExhaust();
+            obj.PuppetEventHandler += PuppetStateChanged;
+            
+            this.SetObjParamsTo(obj);
+            DA.SetData(0, obj);
+        }
+        
+    }
+}
