939662e92b22346112693796701ae48b42caf7ec
Mingbo Peng <mingo1214@gmail.com>
2019-03-19 20:00:11 -0400

    added setpointManagerOutdoorAirPretreat, added SPM to OutdoorAirSystem's stream

--- src/Ironbug.HVAC/LoopObjs/IB_OutdoorAirSystem.cs
+++ src/Ironbug.HVAC/LoopObjs/IB_OutdoorAirSystem.cs
@@ -53,10 +53,13 @@ namespace Ironbug.HVAC
             oa.addToNode(node);
             var oaNode = oa.outboardOANode().get();
             var oaObjs = this.OAStreamObjs.Reverse();
-            foreach (var item in oaObjs)
+
+            var comps = oaObjs.Where(_ => !(_ is IB_SetpointManager));
+            foreach (var item in comps)
             {
-                item.ToOS(model).addToNode(oaNode);
+                item.AddToNode(oaNode);
             };
+            AddSetPoints(oaNode, oaObjs);
 
             var rfNode = oa.outboardReliefNode().get();
             var rfObjs = this.ReliefStreamObjs;
@@ -101,13 +104,81 @@ namespace Ironbug.HVAC
             return newObj;
         }
 
-        //public override ModelObject ToOS(Model model)
-        //{
-        //    return base.ToOS(model);
-        //}
-        //public void ToOS_Demand(Loop Loop)
-        //{
-        //    this.ToOS()
-        //}
+        protected bool AddSetPoints(Node startingNode, IEnumerable<IB_HVACObject> components)
+        {
+
+            var Loop = startingNode.loop().get();
+            
+            var currentComps = Loop.to_AirLoopHVAC().get().airLoopHVACOutdoorAirSystem().get().oaComponents();
+            var allTrackingIDs = currentComps.Select(_ => _.comment()).ToList();
+
+            var setPts = components.Where(_ => _ is IB_SetpointManager);
+
+            //check if there is set point
+            if (setPts.Count() == 0)
+            {
+                return true;
+            }
+
+            int added = 0;
+
+            //check if there is only one component and it is set point.
+            if (setPts.Count() == components.Count())
+            {
+                foreach (var item in setPts)
+                {
+                    added = item.AddToNode(startingNode) ? added + 1 : added;
+                }
+                return true;
+            }
+
+            //check if set point is at the first
+            IEnumerable<IB_HVACObject> remainingSetPts = null;
+            if (components.First() is IB_SetpointManager)
+            {
+                added = setPts.First().AddToNode(startingNode) ? added + 1 : added;
+                remainingSetPts = setPts.Skip(1);
+            }
+            else
+            {
+                remainingSetPts = setPts;
+            }
+
+            //until now, set point can only be at middle or the last
+            foreach (var item in remainingSetPts)
+            {
+                //var setPt = (IB_SetpointManager)item;
+                var atIndex = components.ToList().IndexOf(item);
+
+                OptionalNode nodeWithSetPt = null;
+
+                //Find the component before setpoint
+                var comBeforeSetPt = components.ElementAt(atIndex - 1);
+                var names = currentComps.Select(_ => _.nameString()).ToList();
+                var nodeName = startingNode.nameString();
+                var indexOfStartingNode = names.IndexOf(nodeName);
+               
+                var comBeforeSetPt_ID = comBeforeSetPt.GetTrackingID();
+                
+                var combeforeSetPt_Index = allTrackingIDs.IndexOf(comBeforeSetPt_ID);
+
+                //Find the node for setPoint
+                var node_Index = indexOfStartingNode + combeforeSetPt_Index - 1;
+                nodeWithSetPt = currentComps.ElementAt(node_Index).to_Node();
+              
+
+                //Add to the node
+                added = item.AddToNode(nodeWithSetPt.get()) ? added + 1 : added;
+            }
+
+            var allcopied = added == setPts.Count();
+
+            if (!allcopied)
+            {
+                throw new Exception("Failed to add all set point managers!");
+            }
+
+            return allcopied;
+        }
     }
 }
