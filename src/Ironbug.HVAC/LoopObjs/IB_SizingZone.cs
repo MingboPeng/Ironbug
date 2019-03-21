08ce85a88eb23f160e1b63cad248f78b19d0884a
Mingbo Peng <mingo1214@gmail.com>
2019-03-20 20:08:44 -0400

    sizingZone ToOS

--- src/Ironbug.HVAC/LoopObjs/IB_SizingZone.cs
+++ src/Ironbug.HVAC/LoopObjs/IB_SizingZone.cs
@@ -20,8 +20,11 @@ namespace Ironbug.HVAC
         public ModelObject ToOS(ThermalZone thermalZone)
         {
             //create a sizingZone to target thermalZone
-            var targetModel = thermalZone.model();
-            return base.OnNewOpsObj((Model model)=> new SizingZone(model, thermalZone), targetModel);
+            var sz = thermalZone.sizingZone();
+            sz.SetCustomAttributes(this.CustomAttributes);
+            return sz;
+            //var targetModel = thermalZone.model();
+            //return base.OnNewOpsObj((Model model)=> new SizingZone(model, thermalZone), targetModel);
         }
         
         
