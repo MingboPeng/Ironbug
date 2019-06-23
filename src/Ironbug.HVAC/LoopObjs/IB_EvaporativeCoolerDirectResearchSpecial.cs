8b42af0f1c9d0caf27f71cd5cdea7be1b60fa74c
Mingbo Peng <mingo1214@gmail.com>
2019-06-23 18:55:54 -0400

    added EvaporativeFluidCoolers, EvaporativeCoolers

---
+++ src/Ironbug.HVAC/LoopObjs/IB_EvaporativeCoolerDirectResearchSpecial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+
+namespace Ironbug.HVAC
+{
+    public class IB_EvaporativeCoolerDirectResearchSpecial : IB_HVACObject, IIB_AirLoopObject
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_EvaporativeCoolerDirectResearchSpecial();
+
+        private static EvaporativeCoolerDirectResearchSpecial NewDefaultOpsObj(Model model) => new EvaporativeCoolerDirectResearchSpecial(model, model.alwaysOnDiscreteSchedule());
+        public IB_EvaporativeCoolerDirectResearchSpecial() : base(NewDefaultOpsObj(new Model()))
+        {
+        }
+        
+        public override HVACComponent ToOS(Model model)
+        {
+            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+        }
+    }
+
+    public sealed class IB_EvaporativeCoolerDirectResearchSpecial_FieldSet
+        : IB_FieldSet<IB_EvaporativeCoolerDirectResearchSpecial_FieldSet, EvaporativeCoolerDirectResearchSpecial>
+    {
+
+        private IB_EvaporativeCoolerDirectResearchSpecial_FieldSet() { }
+
+
+    }
+}
---
+++ src/Ironbug.HVAC/LoopObjs/IB_EvaporativeFluidCoolerTwoSpeed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+
+namespace Ironbug.HVAC
+{
+    public class IB_EvaporativeFluidCoolerTwoSpeed : IB_HVACObject, IIB_PlantLoopObjects
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_EvaporativeFluidCoolerTwoSpeed();
+
+        private static EvaporativeFluidCoolerTwoSpeed NewDefaultOpsObj(Model model) => new EvaporativeFluidCoolerTwoSpeed(model);
+        public IB_EvaporativeFluidCoolerTwoSpeed() : base(NewDefaultOpsObj(new Model()))
+        {
+        }
+        
+        public override HVACComponent ToOS(Model model)
+        {
+            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+        }
+    }
+
+    public sealed class IB_EvaporativeFluidCoolerTwoSpeed_FieldSet
+        : IB_FieldSet<IB_EvaporativeFluidCoolerTwoSpeed_FieldSet, EvaporativeFluidCoolerTwoSpeed>
+    {
+
+        private IB_EvaporativeFluidCoolerTwoSpeed_FieldSet() { }
+
+
+    }
+}
