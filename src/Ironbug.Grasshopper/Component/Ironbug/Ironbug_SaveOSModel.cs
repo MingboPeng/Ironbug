10c99621cbb0431ac3b7c0b57b1acd44c517ef33
Mingbo Peng <mingo1214@gmail.com>
2019-06-29 19:46:30 -0400

    updated Ironbug_SaveToFile

--- src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_SaveOSModel.cs
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_SaveOSModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Grasshopper.Kernel;
 using System.IO;
 
@@ -8,7 +7,7 @@ namespace Ironbug.Grasshopper.Component
     public class Ironbug_SaveOSModel : Ironbug_Component
     {
         protected override System.Drawing.Bitmap Icon => Properties.Resources.saveHVAC; 
-        public override Guid ComponentGuid => new Guid("3246f516-d4cf-45e0-b0a7-abb47bb014c1");
+        public override Guid ComponentGuid => new Guid("{2B473359-4DFC-4DE7-BD3E-79C119C64250}");
         
         public Ironbug_SaveOSModel()
           : base("Ironbug_SaveToFile", "SaveToFile",
@@ -19,20 +18,13 @@ namespace Ironbug.Grasshopper.Component
 
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddGenericParameter("NewFilePath", "path", "New OSM file path. This file will be deleted first if it is existed", GH_ParamAccess.item);
-            pManager.AddGenericParameter("AirLoops", "AirLoops", "Zone with HVAC system set", GH_ParamAccess.list);
-            pManager.AddGenericParameter("PlantLoops", "PlantLoops", "PlantLoops", GH_ParamAccess.list);
-            pManager.AddGenericParameter("VRFSystems", "VRFSystems", "VRFSystems", GH_ParamAccess.list);
+            pManager.AddTextParameter("NewFilePath", "_OSMpath", "New OSM file path. This file will be deleted first if it is existed", GH_ParamAccess.item);
+            pManager.AddGenericParameter("HVACSystem", "_HVAC", "A HVAC system from Ironbug_HVACSystem", GH_ParamAccess.item);
             pManager.AddBooleanParameter("Write", "_write", "Write the OpenStudio file.", GH_ParamAccess.item, false);
 
             pManager[0].Optional = true;
             pManager[1].Optional = true;
-            pManager[1].DataMapping = GH_DataMapping.Flatten;
             pManager[2].Optional = true;
-            pManager[2].DataMapping = GH_DataMapping.Flatten;
-            pManager[3].Optional = true;
-            pManager[3].DataMapping = GH_DataMapping.Flatten;
-
         }
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
@@ -43,20 +35,12 @@ namespace Ironbug.Grasshopper.Component
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             string filepath = string.Empty;
-            //filepath = @"C:\Users\mingo\Documents\GitHub\Ironbug\doc\osmFile\savedFromGH.osm";
-            var airLoops = new List<HVAC.IB_AirLoopHVAC>();
-            var plantLoops = new List<HVAC.IB_PlantLoop>();
-            var vrfs = new List<HVAC.IB_AirConditionerVariableRefrigerantFlow>();
+            HVAC.IB_HVACSystem hvac = null;
             bool write = false;
 
-            //var model = new OpenStudio.Model();
-
             DA.GetData(0, ref filepath);
-            DA.GetDataList(1, airLoops);
-            DA.GetDataList(2,  plantLoops);
-            DA.GetDataList(3, vrfs);
-
-            DA.GetData(4, ref write);
+            DA.GetData(1, ref hvac);
+            DA.GetData(2, ref write);
 
             if (!write) return;
             
@@ -64,15 +48,13 @@ namespace Ironbug.Grasshopper.Component
             if (File.Exists(filepath))
             {
                 File.Delete(filepath);
-            }
-            var hvac = new HVAC.IB_HVACSystem(airLoops, plantLoops, vrfs);
+            } 
             var saved = hvac.SaveHVAC(filepath);
 
             if (saved)
             {
                 DA.SetData(0, filepath);
             }
-            
             
             
         }
---
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_SaveOSModel_Obsolete.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using System.IO;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_SaveOSModel_Obsolete : Ironbug_Component
+    {
+        public override bool Obsolete => true;
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.saveHVAC; 
+        public override Guid ComponentGuid => new Guid("3246f516-d4cf-45e0-b0a7-abb47bb014c1");
+        
+        public Ironbug_SaveOSModel_Obsolete()
+          : base("Ironbug_SaveToFile", "SaveToFile",
+              "Description",
+              "Ironbug", "HVAC")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("NewFilePath", "path", "New OSM file path. This file will be deleted first if it is existed", GH_ParamAccess.item);
+            pManager.AddGenericParameter("AirLoops", "AirLoops", "Zone with HVAC system set", GH_ParamAccess.list);
+            pManager.AddGenericParameter("PlantLoops", "PlantLoops", "PlantLoops", GH_ParamAccess.list);
+            pManager.AddGenericParameter("VRFSystems", "VRFSystems", "VRFSystems", GH_ParamAccess.list);
+            pManager.AddBooleanParameter("Write", "_write", "Write the OpenStudio file.", GH_ParamAccess.item, false);
+
+            pManager[0].Optional = true;
+            pManager[1].Optional = true;
+            pManager[1].DataMapping = GH_DataMapping.Flatten;
+            pManager[2].Optional = true;
+            pManager[2].DataMapping = GH_DataMapping.Flatten;
+            pManager[3].Optional = true;
+            pManager[3].DataMapping = GH_DataMapping.Flatten;
+
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("FilePath", "path", "file path", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string filepath = string.Empty;
+            //filepath = @"C:\Users\mingo\Documents\GitHub\Ironbug\doc\osmFile\savedFromGH.osm";
+            var airLoops = new List<HVAC.IB_AirLoopHVAC>();
+            var plantLoops = new List<HVAC.IB_PlantLoop>();
+            var vrfs = new List<HVAC.IB_AirConditionerVariableRefrigerantFlow>();
+            bool write = false;
+
+            //var model = new OpenStudio.Model();
+
+            DA.GetData(0, ref filepath);
+            DA.GetDataList(1, airLoops);
+            DA.GetDataList(2,  plantLoops);
+            DA.GetDataList(3, vrfs);
+
+            DA.GetData(4, ref write);
+
+            if (!write) return;
+            
+            if (string.IsNullOrEmpty(filepath)) return;
+            if (File.Exists(filepath))
+            {
+                File.Delete(filepath);
+            }
+            var hvac = new HVAC.IB_HVACSystem(airLoops, plantLoops, vrfs);
+            var saved = hvac.SaveHVAC(filepath);
+
+            if (saved)
+            {
+                DA.SetData(0, filepath);
+            }
+            
+            
+            
+        }
+
+
+
+    }
+}
