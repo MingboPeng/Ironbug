d3179d43c52b9422ea65ee5312498af1413f86b9
Mingbo Peng <mingo1214@gmail.com>
2019-08-07 21:01:17 -0400

    Adjusted and added output for HeatExchangerFluid

--- src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_HeatExchangerFluidToFluid_Obsolete.cs
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_HeatExchangerFluidToFluid_Obsolete.cs
@@ -3,19 +3,20 @@ using Grasshopper.Kernel;
 
 namespace Ironbug.Grasshopper.Component
 {
-    public class Ironbug_HeatExchangerFluidToFluid : Ironbug_DuplicableHVACWithParamComponent
+    public class Ironbug_HeatExchangerFluidToFluid_Obsolete : Ironbug_DuplicableHVACWithParamComponent
     {
-        public Ironbug_HeatExchangerFluidToFluid()
-          : base("Ironbug_HeatExchangerFluidToFluid", "HeatExchangerFluid",
+        public Ironbug_HeatExchangerFluidToFluid_Obsolete()
+          : base("Ironbug_HeatExchangerFluidToFluid_Obsolete", "HeatExchangerFluid",
               "Description",
               "Ironbug", "02:LoopComponents",
               typeof(HVAC.IB_HeatExchangerFluidToFluid_FieldSet))
         {
         }
-
+        public override bool Obsolete => true;
+    
         protected override System.Drawing.Bitmap Icon => Properties.Resources.HXFluid;
-        public override Guid ComponentGuid => new Guid("{B2640599-4B0B-4B47-A3F7-4A7C83C4E0FE}");
-        public override GH_Exposure Exposure => GH_Exposure.quarternary;
+        public override Guid ComponentGuid => new Guid("6A078A99-218A-449D-9E5F-9F8B6E86A0D6");
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
 
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
@@ -23,8 +24,7 @@ namespace Ironbug.Grasshopper.Component
         
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddGenericParameter("HeatExchangerFluidToFluid", "AtDemand", "HeatExchangerFluidToFluid connects to source plantloop demand side.", GH_ParamAccess.item);
-            pManager.AddGenericParameter("HeatExchangerFluidToFluid", "AtSupply", "HeatExchangerFluidToFluid connects to another plantloop supply side.", GH_ParamAccess.item);
+            pManager.AddGenericParameter("HeatExchangerFluidToFluid", "HXFluid", "HeatExchangerFluidToFluid", GH_ParamAccess.item);
         }
         protected override void SolveInstance(IGH_DataAccess DA)
         {
@@ -33,7 +33,6 @@ namespace Ironbug.Grasshopper.Component
             this.SetObjParamsTo(obj);
             var objs = this.SetObjDupParamsTo(obj);
             DA.SetDataList(0, objs);
-            DA.SetDataList(1, objs);
         }
 
 
