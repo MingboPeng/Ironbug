6c8e70dbab05a9318d641318b01a0aea19818908
MingboPeng <mingo1214@gmail.com>
2020-05-23 19:46:46 -0400

    refactor(LBHB): separate all LBHB files to LBHB_Legacy project

---
+++ src/Ironbug.LBHB_Legacy/Archived/archived.cs
@@ -0,0 +1,39 @@
+﻿using Grasshopper.Kernel;
+using System;
+
+namespace Ironbug.LBHB_Legacy.Archived
+{
+    public class Ladybug_ImageViewer_OBSOLETE : Ladybug_ImageViewer
+    {
+        public override Guid ComponentGuid => new Guid("a5359d3f-336b-443b-98b7-7903b6cc0c47");
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
+
+
+    }
+    public class Ladybug_ImageViewer2_OBSOLETE : Ladybug_ImageViewer
+    {
+        public override Guid ComponentGuid => new Guid("949F869E-E51E-4776-BF60-1C54760C453F");
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
+
+    }
+
+    public class Ladybug_SortByLayers_OBSOLETE : Ladybug_SortByLayers
+    {
+        public override Guid ComponentGuid => new Guid("a3b74b0f-2fab-42de-bd88-3326331a93a5");
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
+
+    }
+
+    public class Honeybee_ReadAnnualResultsIII_OBSOLETE : Honeybee_ReadAnnualResultsIII
+    {
+        public override Guid ComponentGuid => new Guid("a5df3302-a8b7-468b-8ff8-9d5ceaa9bc1b");
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
+
+    }
+    public class Honeybee_IntersectMassII_OBSOLETE : Honeybee_IntersectMassII
+    {
+        public override Guid ComponentGuid => new Guid("1AB1248B-4531-4270-9A0B-DB3EBC2581A6");
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
+
+    }
+}
---
+++ src/Ironbug.LBHB_Legacy/IronbugLBHB_LegacyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Ironbug.LBHB_Legacy
+{
+    public class IronbugLBHB_LegacyInfo : GH_AssemblyInfo
+   {
+        public override string Name => "Ironbug.LBHB_Legacy";
+        public override Bitmap Icon => null;
+        public override string Description => "Ironbug components for ladybug tools";
+        public override Guid Id => new Guid("7a16f00b-ff8f-49c8-bdc6-7d6524ac872a");
+
+        public override string AuthorName => "Mingbo Peng";
+        public override string AuthorContact => "Mingbo@alumni.upenn.edu";
+    }
+}
