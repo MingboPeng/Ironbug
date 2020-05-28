a9e464f6e0f5a1274ee21ba114bfb19da9623a5c
MingboPeng <mingo1214@gmail.com>
2020-05-28 11:37:52 -0400

    feat(LoopObjs): add PlantComponentTemperatureSource

---
+++ src/Ironbug.HVAC/LoopObjs/IB_PlantComponentTemperatureSource.cs
@@ -0,0 +1,28 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+
+namespace Ironbug.HVAC
+{
+    public class IB_PlantComponentTemperatureSource: IB_HVACObject, IIB_PlantLoopObjects
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_PlantComponentTemperatureSource();
+
+        private static PlantComponentTemperatureSource NewDefaultOpsObj(Model model) => new PlantComponentTemperatureSource(model);
+        public IB_PlantComponentTemperatureSource() : base(NewDefaultOpsObj(new Model()))
+        {
+        }
+
+        public override HVACComponent ToOS(Model model)
+        {
+            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+        }
+    }
+    public sealed class IB_PlantComponentTemperatureSource_FieldSet
+        : IB_FieldSet<IB_PlantComponentTemperatureSource_FieldSet, PlantComponentTemperatureSource>
+    {
+        private IB_PlantComponentTemperatureSource_FieldSet() { }
+    }
+
+}
