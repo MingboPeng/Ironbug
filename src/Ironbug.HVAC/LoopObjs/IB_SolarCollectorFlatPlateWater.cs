b8c47439cee6f8745f68972a711f9453932909fb
jessica-chen2-wsp <107937808+jessica-chen2-wsp@users.noreply.github.com>
2022-06-24 16:08:46 -0400

    0624 update

--- src/Ironbug.HVAC/LoopObjs/IB_SolarCollectorFlatPlateWater.cs
+++ src/Ironbug.HVAC/LoopObjs/IB_SolarCollectorFlatPlateWater.cs
@@ -9,12 +9,23 @@ namespace Ironbug.HVAC
         protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_SolarCollectorFlatPlateWater();
 
         private static SolarCollectorFlatPlateWater NewDefaultOpsObj(Model model) => new SolarCollectorFlatPlateWater(model);
+        private IB_PlanarSurface _surface => this.GetChild<IB_PlanarSurface>(0);
         public IB_SolarCollectorFlatPlateWater() : base(NewDefaultOpsObj(new Model()))
         {
+            this.AddChild(null);
+        }
+        public void SetSurface(IB_PlanarSurface surface)
+        {
+            this.SetChild(0, surface);
         }
         public override HVACComponent ToOS(Model model)
         {
-            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+
+            var opsObj = base.OnNewOpsObj(NewDefaultOpsObj, model);
+            //probably should change input from string to surface - Question: how does one convert HB surface to OS surface
+            if (this._surface != null) opsObj.setSurface(this._surface);
+
+            return opsObj;
         }
     }
 
