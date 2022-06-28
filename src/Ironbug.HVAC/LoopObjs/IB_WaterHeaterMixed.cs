df883f3cf2d981a507367e1c566ee1aaa0a656bf
jessica-chen2-wsp <107937808+jessica-chen2-wsp@users.noreply.github.com>
2022-06-28 14:42:43 -0400

    0618update
    added Ambient Temperature Thermal Zone parameter to WaterHeaterMixed

--- src/Ironbug.HVAC/LoopObjs/IB_WaterHeaterMixed.cs
+++ src/Ironbug.HVAC/LoopObjs/IB_WaterHeaterMixed.cs
@@ -6,15 +6,29 @@ namespace Ironbug.HVAC
 {
     public class IB_WaterHeaterMixed : IB_HVACObject, IIB_PlantLoopObjects
     {
-        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_WaterHeaterMixed();
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_WaterHeaterMixed(this._Zone);
 
         private static WaterHeaterMixed NewDefaultOpsObj(Model model) => new WaterHeaterMixed(model);
-        public IB_WaterHeaterMixed() : base(NewDefaultOpsObj(new Model()))
+        private IB_ThermalZone _Zone => this.GetChild<IB_ThermalZone>();
+        public IB_WaterHeaterMixed(IB_ThermalZone ThermalZone) : base(NewDefaultOpsObj(new Model()))
         {
+            this.AddChild(ThermalZone);
+        }
+        //constructor for no ambient temperature zone option
+        public IB_WaterHeaterMixed()
+            : this(null)
+        {
+        }
+        public void setAmbientTemperatureThermalZone(IB_ThermalZone Zone)
+        {
+            this.SetChild(0, Zone);
         }
         public override HVACComponent ToOS(Model model)
         {
-            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+            var opsObj = base.OnNewOpsObj(NewDefaultOpsObj, model);
+            if (this._Zone != null)
+                opsObj.setAmbientTemperatureThermalZone(this._Zone.ToOS(model) as ThermalZone);
+            return opsObj;
         }
     }
 
