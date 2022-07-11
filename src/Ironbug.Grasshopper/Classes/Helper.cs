d6b3a7a6b976c7d55cdbbd9e0c3cadd526b34763
MingboPeng <mingo1214@gmail.com>
2022-07-10 21:16:56 -0400

    fix(HBRooms): support Pollination Rooms

--- src/Ironbug.Grasshopper/Classes/Helper.cs
+++ src/Ironbug.Grasshopper/Classes/Helper.cs
@@ -6,6 +6,57 @@ namespace Ironbug.Grasshopper
 {
     public static class Helper
     {
+
+        public static IEnumerable<string> GetRoomNames(List<object> HBZonesOrNames)
+        {
+            var firstItem = HBZonesOrNames?.FirstOrDefault();
+            var zoneNames = new List<string>();
+
+            if (firstItem == null)
+                return zoneNames;
+
+            if (firstItem is Types.OsZone)
+            {
+                zoneNames = HBZonesOrNames.Where(_ => _ is Types.OsZone).Select(_ => (_ as Types.OsZone).ZoneName).ToList<string>();
+            }
+            else if (firstItem is GH_Brep)
+            {
+                // Legacy HBZones
+                var hbzones = HBZonesOrNames.SkipWhile(_ => _ is null || _ is Types.OsZone).Select(_ => _ as GH_Brep);
+                zoneNames = Helper.CallFromHBHive(hbzones).ToList();
+            }
+            else if (firstItem is GH_String)
+            {
+                zoneNames = HBZonesOrNames.Select(_ => (_ as GH_String).Value).ToList<string>();
+            }
+            else if (firstItem is GH_ObjectWrapper wrapper)
+            {
+                // LBT Room
+                var isLBTRoom = wrapper.Value.ToString().StartsWith("Room:");
+                isLBTRoom &= wrapper.Value.GetType().ToString().StartsWith("IronPython.");
+
+                if (isLBTRoom)
+                {
+                    zoneNames = Helper.FromLBTRooms(HBZonesOrNames).ToList();
+                }
+
+            }
+            else if (firstItem.GetType().Name == "GH_HBPythonObjectGoo") //Pollination Honeybee components
+            {
+                var rm = HBZonesOrNames[0];
+                var tp = rm?.GetType();
+                var type = tp?.GetProperty("POTypeName")?.GetValue(rm)?.ToString();
+
+                if (type == "Room")
+                {
+                    var pyObjs = HBZonesOrNames.Select(_ => tp.GetProperty("RefPythonObj")?.GetValue(rm)).Select(_ => new GH_ObjectWrapper(_));
+                    zoneNames = Helper.FromLBTRooms(pyObjs).ToList();
+                }
+            }
+
+            return zoneNames;
+        }
+
         /// For new Honeybee
         public static IEnumerable<string> FromLBTRooms(IEnumerable<object> inRooms)
         {
