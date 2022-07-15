b19a7bb519c75a1af5845e3edd1f2730d5d76a3e
jessica-chen2-wsp <107937808+jessica-chen2-wsp@users.noreply.github.com>
2022-07-15 10:27:44 -0400

    0715 sync

---
+++ src/Ironbug.HVAC/LoopObjs/IB_PhotovoltaicPerformanceSimple.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+
+namespace Ironbug.HVAC
+{
+    public class IB_PhotovoltaicPerformanceSimple : IB_ModelObject
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_PhotovoltaicPerformanceSimple();
+
+        private static PhotovoltaicPerformanceSimple NewDefaultOpsObj(Model model) => new PhotovoltaicPerformanceSimple(model);
+        public IB_PhotovoltaicPerformanceSimple() : base(NewDefaultOpsObj(new Model()))
+        {
+        }
+        public void setAmbientTemperatureThermalZone()
+        {
+        }
+        public override IB_ModelObject ToOS(Model model)
+        {
+            return opsObj = base.OnNewOpsObj(NewDefaultOpsObj, model);
+        }
+    }
+
+    public sealed class IB_PhotovoltaicPerformanceSimple_FieldSet
+        : IB_FieldSet<IB_PhotovoltaicPerformanceSimple_FieldSet, PhotovoltaicPerformanceSimple>
+    {
+        private IB_PhotovoltaicPerformanceSimple_FieldSet() { }
+    }
+}
