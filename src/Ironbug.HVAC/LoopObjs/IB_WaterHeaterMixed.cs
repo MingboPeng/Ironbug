d34aa37e2e25972ee24880d9b2287fe22b519be9
peter-wildfeuer-wsp <peter.wildfeuer@wsp.com>
2022-07-18 11:19:10 -0400

    returning to hwhp to zone equipment

--- src/Ironbug.HVAC/LoopObjs/IB_WaterHeaterMixed.cs
+++ src/Ironbug.HVAC/LoopObjs/IB_WaterHeaterMixed.cs
@@ -6,28 +6,30 @@ namespace Ironbug.HVAC
 {
     public class IB_WaterHeaterMixed : IB_HVACObject, IIB_PlantLoopObjects
     {
-        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_WaterHeaterMixed(this._Zone);
-
+        //protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_WaterHeaterMixed(this._Zone);
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_WaterHeaterMixed();
         private static WaterHeaterMixed NewDefaultOpsObj(Model model) => new WaterHeaterMixed(model);
-        private IB_ThermalZone _Zone => this.GetChild<IB_ThermalZone>();
-        public IB_WaterHeaterMixed(IB_ThermalZone ThermalZone) : base(NewDefaultOpsObj(new Model()))
-        {
-            this.AddChild(ThermalZone);
-        }
+        //private IB_ThermalZone _Zone => this.GetChild<IB_ThermalZone>();
+        //public IB_WaterHeaterMixed(IB_ThermalZone ThermalZone) : base(NewDefaultOpsObj(new Model()))
+        //{
+        //    this.AddChild(ThermalZone);
+        //}
         //constructor for no ambient temperature zone option
-        public IB_WaterHeaterMixed()
-            : this(null)
-        {
+        //public IB_WaterHeaterMixed()
+        //    : this(null)
+        //{ }
+
+        public IB_WaterHeaterMixed() : base(NewDefaultOpsObj(new Model())) {
         }
-        public void setAmbientTemperatureThermalZone(IB_ThermalZone Zone)
-        {
-            this.SetChild(0, Zone);
-        }
+        //public void setAmbientTemperatureThermalZone(IB_ThermalZone Zone)
+        //{
+        //    this.SetChild(0, Zone);
+        //}
         public override HVACComponent ToOS(Model model)
         {
             var opsObj = base.OnNewOpsObj(NewDefaultOpsObj, model);
-            if (this._Zone != null)
-                opsObj.setAmbientTemperatureThermalZone(this._Zone.ToOS(model) as ThermalZone);
+            //if (this._Zone != null)
+            //    opsObj.setAmbientTemperatureThermalZone(this._Zone.ToOS(model) as ThermalZone);
             return opsObj;
         }
     }
