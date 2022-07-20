2d61713e233edf117d3327ec8dad0b6ca7b0e0fe
jessica-chen2-wsp <107937808+jessica-chen2-wsp@users.noreply.github.com>
2022-07-20 16:17:44 -0400

    07-20 update

--- src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_CoilCoolingDXTwoStageWithHumidityControlMode.cs
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_CoilCoolingDXTwoStageWithHumidityControlMode.cs
@@ -4,16 +4,16 @@ using Grasshopper.Kernel;
 
 namespace Ironbug.Grasshopper.Component
 {
-    public class Ironbug_CoilCoolingDXTwoSpeed : Ironbug_DuplicableHVACWithParamComponent
+    public class Ironbug_CoilCoolingDXTwoStageWithHumidityControlMode : Ironbug_DuplicableHVACWithParamComponent
     {
         
         /// Initializes a new instance of the Ironbug_ChillerElectricEIR class.
         
-        public Ironbug_CoilCoolingDXTwoSpeed()
-          : base("IB_CoilCoolingDXTwoSpeed", "CoilClnDX2",
+        public Ironbug_CoilCoolingDXTwoStageWithHumidityControlMode()
+          : base("IB_CoilCoolingDXTwoStageWithHumidityControlMode", "CoilClnDX3",
               "Description",
               "Ironbug", "02:LoopComponents",
-              typeof(HVAC.IB_CoilCoolingDXTwoSpeed_FieldSet))
+              typeof(HVAC.IB_CoilCoolingDXTwoStageWithHumidityControlMode_FieldSet))
         {
         }
 
@@ -24,13 +24,13 @@ namespace Ironbug.Grasshopper.Component
         }
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddGenericParameter("CoilCoolingDXTwoSpeed", "Coil", "CoilCoolingDXTwoSpeed", GH_ParamAccess.item);
+            pManager.AddGenericParameter("CoilCoolingDXTwoStageWithHumidityControlMode", "Coil", "CoilCoolingDXTwoStageWithHumidityControlMode", GH_ParamAccess.item);
         }
 
         
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            var obj = new HVAC.IB_CoilCoolingDXTwoSpeed();
+            var obj = new HVAC.IB_CoilCoolingDXTwoStageWithHumidityControlMode();
 
             this.SetObjParamsTo(obj);
             var objs = this.SetObjDupParamsTo(obj);
@@ -38,8 +38,9 @@ namespace Ironbug.Grasshopper.Component
         }
 
 
-        protected override System.Drawing.Bitmap Icon => Properties.Resources.CoilCDX2;
+        //protected override System.Drawing.Bitmap Icon => Properties.Resources.CoilCDX2;
 
-        public override Guid ComponentGuid => new Guid("5237B52C-06E2-485B-B414-9A3455DA2A72");
+        public override Guid ComponentGuid => new Guid("9ACF588F-DAD7-4D6A-BB54-E8EC674B550A");
+
     }
 }
