dc6e8884e67f622d9456314ab17f2634972e6ef9
MingboPeng <mingo1214@gmail.com>
2023-01-06 16:40:30 +0800

    fix(PTAC): support Electric and Gas heating coil

--- src/Ironbug.HVAC/ZoneEquipments/ZoneHVAC/IB_ZoneHVACPackagedTerminalAirConditioner.cs
+++ src/Ironbug.HVAC/ZoneEquipments/ZoneHVAC/IB_ZoneHVACPackagedTerminalAirConditioner.cs
@@ -8,15 +8,15 @@ namespace Ironbug.HVAC
     {
         protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_ZoneHVACPackagedTerminalAirConditioner(_fan, _heatingCoil, _coolingCoil);
 
-        private static ZoneHVACPackagedTerminalAirConditioner NewDefaultOpsObj(Model model, IB_Fan SupplyFan, IB_Coil HeatingCoil, IB_Coil CoolingCoil) 
+        private static ZoneHVACPackagedTerminalAirConditioner NewDefaultOpsObj(Model model, IB_Fan SupplyFan, IB_CoilHeatingBasic HeatingCoil, IB_Coil CoolingCoil) 
             => new ZoneHVACPackagedTerminalAirConditioner(model,model.alwaysOnDiscreteSchedule(), SupplyFan.ToOS(model), HeatingCoil.ToOS(model), CoolingCoil.ToOS(model));
 
         private IB_Coil _coolingCoil => this.GetChild<IB_Coil>(0);
-        private IB_Coil _heatingCoil => this.GetChild<IB_Coil>(1);
+        private IB_CoilHeatingBasic _heatingCoil => this.GetChild<IB_CoilHeatingBasic>(1);
         private IB_Fan _fan => this.GetChild<IB_Fan>(2);
 
         private IB_ZoneHVACPackagedTerminalAirConditioner() : base(null) { }
-        public IB_ZoneHVACPackagedTerminalAirConditioner(IB_Fan SupplyFan, IB_Coil HeatingCoil, IB_Coil CoolingCoil) 
+        public IB_ZoneHVACPackagedTerminalAirConditioner(IB_Fan SupplyFan, IB_CoilHeatingBasic HeatingCoil, IB_Coil CoolingCoil) 
             : base(NewDefaultOpsObj(new Model(),SupplyFan, HeatingCoil, CoolingCoil))
         {
             this.AddChild(CoolingCoil);
