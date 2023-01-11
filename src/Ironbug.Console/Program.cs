ad347308ab796a323c9ccb26e8218e88f0f5ad7c
MingboPeng <mingo1214@gmail.com>
2023-01-11 17:17:38 +0800

    fix(Console): fix Console with OpenStudio assembly

--- src/Ironbug.Console/Program.cs
+++ src/Ironbug.Console/Program.cs
@@ -1,46 +1,65 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 
 namespace Ironbug
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-
-            var commandArgs = args.Select(x => x.Trim()).Where(_ => !string.IsNullOrEmpty(_));
-
-            if (!commandArgs.Any() || commandArgs.Count() != 2)
+            try
             {
-                Console.WriteLine("Hello, this is Ironbug Console app!");
-                return;
+                AppDomain currentDomain = AppDomain.CurrentDomain;
+                currentDomain.AssemblyResolve += new ResolveEventHandler(OpsResolveEventHandler);
+                
+                var commandArgs = args.Select(x => x.Trim()).Where(_ => !string.IsNullOrEmpty(_));
+
+                if (!commandArgs.Any() || commandArgs.Count() != 2)
+                {
+                    Console.WriteLine("Hello, this is Ironbug Console app!");
+                    return;
+                }
+                var osm = commandArgs.FirstOrDefault();
+                var hvac = commandArgs.LastOrDefault();
+
+                //var osm = @"D:\Dev\Ironbug\src\Ironbug.HVAC_Tests\TestSource\Integration Testing\FourOfficeBuilding - Copy.osm";
+                //var hvac = @"D:\Dev\Ironbug\src\Ironbug.HVAC_Tests\TestSource\Integration Testing\Sys02_PTHP_Advanced.json";
+
+                Console.WriteLine($"[INFO] Input osm file: {osm}");
+                Console.WriteLine($"[INFO] Input ironbug HVAC json file: {hvac}");
+
+                var done = HVAC.IB_HVACSystem.SaveHVAC(osm, hvac);
+                if (done)
+                    Console.WriteLine($"[INFO] HVAC is added to osm file:\n {osm}");
+                else
+                    throw new ArgumentException("Failed to save HVAC to osm file");
             }
-            var osm = commandArgs.FirstOrDefault();
-            var hvac = commandArgs.LastOrDefault();
-
-            //var osm = "in.osm";
-            //var hvac = "hvac.txt";
-
-            Console.WriteLine($"Input osm file: {osm}");
-            Console.WriteLine($"Input ironbug HVAC json file: {hvac}");
-
-            if (!System.IO.File.Exists(osm) || !osm.ToLower().EndsWith(".osm"))
-                throw new ArgumentException("Invalid osm file");
-            if (!System.IO.File.Exists(hvac))
-                throw new ArgumentException("Invalid hvac file");
-
-            var hvacJson = System.IO.File.ReadAllText(hvac);
-            var system = Ironbug.HVAC.IB_HVACSystem.FromJson(hvacJson);
-
-            if (system != null) Console.WriteLine($"HVAC is valid");
-
-            var done = system?.SaveHVAC(osm);
-            if (done.GetValueOrDefault())
-                Console.WriteLine($"HVAC is added to osm file: {osm}");
-            else
-                throw new ArgumentException("Failed to save HVAC to osm file");
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw e;
+                //Console.ReadLine();
+            }
 
             //Console.ReadLine();
+        }
+
+
+        private static Assembly OpsResolveEventHandler(object sender, ResolveEventArgs args)
+        {
+
+            if (args.Name.StartsWith( "OpenStudio"))
+            {
+                Ironbug.Core.OpenStudio.OpenStudioHelper.LoadAssemblies((string s) => Console.WriteLine($"[INFO] {s}"), out var ops);
+                return ops;
+            }
+            else
+            {
+                return null;
+            }
+
         }
 
 
