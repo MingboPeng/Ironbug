a422d949f431bbd0a87ce79bf1e0d05b1dc3e642
MingboPeng <mingo1214@gmail.com>
2023-01-11 17:12:52 +0800

    test(*): add integration tests

---
+++ src/Ironbug.HVAC_Tests/IntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using Ironbug.HVAC;
+using NUnit.Framework;
+
+namespace Ironbug.HVACTests
+{
+
+    public class IntegrationTests
+    {
+        [Test]
+        public void Integration()
+        {
+            var folder = Path.Combine( TestHelper.TestSourceFolder, "Integration Testing");
+            var files = Directory.GetFiles(folder);
+            var jsons = files.Where(_ => _.EndsWith(".json"));
+            var osm = files.First(_ => _.EndsWith(".osm"));
+
+            foreach (var hvac in jsons)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(hvac);
+                Console.WriteLine($"Testing {fileName}");
+                var saveAsOsm = Path.Combine(Path.GetTempPath(), $"{fileName}.osm");
+                File.Copy(osm, saveAsOsm, true);
+                var done = IB_HVACSystem.SaveHVAC(saveAsOsm, hvac);
+                Assert.True(done);
+
+            }
+          
+        }
+    }
+}
