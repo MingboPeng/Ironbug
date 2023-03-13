fdb5433cc5fcd035438ca3e73f18bf56db0266ae
MingboPeng <mingo1214@gmail.com>
2023-03-13 21:58:18 +0800

    fix(CoilCoolingDXTwoStageWithHumidityControlMode): finish with CoilPerformanceDXCooling

--- src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_CoilCoolingDXTwoStageWithHumidityControlMode.cs
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_CoilCoolingDXTwoStageWithHumidityControlMode.cs
@@ -4,27 +4,33 @@ using Grasshopper.Kernel;
 
 namespace Ironbug.Grasshopper.Component
 {
-    public class Ironbug_CoilCoolingDXTwoStageWithHumidityControlMode : Ironbug_DuplicableHVACWithParamComponent
+    public class Ironbug_CoilCoolingDXTwoStageWithHumidityControlMode : Ironbug_HVACWithParamComponent
     {
         
-        /// Initializes a new instance of the Ironbug_ChillerElectricEIR class.
-        
         public Ironbug_CoilCoolingDXTwoStageWithHumidityControlMode()
-          : base("IB_CoilCoolingDXTwoStageWithHumidityControlMode", "CoilClnDX3",
+          : base("IB_CoilCoolingDXTwoStageWithHumidityControlMode", "CoilClnDX2_HumdCtrl",
               "Description",
               "Ironbug", "02:LoopComponents",
               typeof(HVAC.IB_CoilCoolingDXTwoStageWithHumidityControlMode_FieldSet))
         {
         }
 
-        public override GH_Exposure Exposure => GH_Exposure.secondary;
+        public override GH_Exposure Exposure => GH_Exposure.secondary | GH_Exposure.obscure;
 
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddGenericParameter("CoilPerformanceN1", "_cPn1_", "Coil performance object which specifies the DX coil performance for stage 1 operation without enhanced dehumidification (normal mode). Must be CoilPerformanceDXCooling", GH_ParamAccess.item);
+            pManager[0].Optional = true;
+            pManager.AddGenericParameter("CoilPerformanceN1+2", "_cPn1+2_", "Coil performance object which specifies the DX coil performance for stage 1+2 operation (both stages active) without enhanced dehumidification (normal mode). Must be CoilPerformanceDXCooling", GH_ParamAccess.item);
+            pManager[1].Optional = true;
+            pManager.AddGenericParameter("CoilPerformanceD1", "_cPd1_", "Coil performance object which specifies the DX coil performance for stage 1 operation with enhanced dehumidification active. Must be CoilPerformanceDXCooling", GH_ParamAccess.item);
+            pManager[2].Optional = true;
+            pManager.AddGenericParameter("CoilPerformanceD1+2", "_cPd1+2_", "Coil performance object which specifies the DX coil performance for stage 1+2 operation (both stages active) with enhanced dehumidification active. Must be CoilPerformanceDXCooling", GH_ParamAccess.item);
+            pManager[3].Optional = true;
         }
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddGenericParameter("CoilCoolingDXTwoStageWithHumidityControlMode", "Coil", "CoilCoolingDXTwoStageWithHumidityControlMode", GH_ParamAccess.item);
+            pManager.AddGenericParameter("Coil", "Coil", "CoilCoolingDXTwoStageWithHumidityControlMode", GH_ParamAccess.item);
         }
 
         
@@ -32,9 +38,18 @@ namespace Ironbug.Grasshopper.Component
         {
             var obj = new HVAC.IB_CoilCoolingDXTwoStageWithHumidityControlMode();
 
+            HVAC.IB_CoilPerformanceDXCooling n1 = null;
+            HVAC.IB_CoilPerformanceDXCooling n12 = null;
+            HVAC.IB_CoilPerformanceDXCooling d1 = null;
+            HVAC.IB_CoilPerformanceDXCooling d12 = null;
+
+            if (DA.GetData(0, ref n1)) obj.SetNormalModeStage1CoilPerformance(n1);
+            if (DA.GetData(1, ref n12)) obj.SetNormalModeStage1Plus2CoilPerformance(n12); 
+            if (DA.GetData(2, ref d1)) obj.SetNormalModeStage1CoilPerformance(d1);
+            if (DA.GetData(3, ref d12)) obj.SetNormalModeStage1Plus2CoilPerformance(d12);
+
             this.SetObjParamsTo(obj);
-            var objs = this.SetObjDupParamsTo(obj);
-            DA.SetDataList(0, objs);
+            DA.SetData(0, obj);
         }
 
 
