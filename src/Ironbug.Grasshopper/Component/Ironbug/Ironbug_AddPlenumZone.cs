d9d9fe09548e69d2079c7148a693022790bb04f3
MingboPeng <mingo1214@gmail.com>
2023-03-16 22:46:36 +0800

    fix(AddPlenumZone): update input names

--- src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_AddPlenumZone.cs
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_AddPlenumZone.cs
@@ -20,17 +20,17 @@ namespace Ironbug.Grasshopper.Component
         
         public Ironbug_AddPlenumZone()
           : base("IB_PlenumZone", "PlenumZone",
-              "Attach supply or/and retrun plenum zones to thermal zones in the airloop.",
+              "Attach supply or/and return plenum zones to thermal zones in the airloop.",
               "Ironbug", "00:Ironbug")
         {
         }
         
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
-            pManager.AddGenericParameter("HoneybeePlenumZone", "supplyPlenum_", "Use HBRoom or OsZones.\nThis plenumZone will be connect to all thermal zones", GH_ParamAccess.list);
+            pManager.AddGenericParameter("supplyPlenum_", "supplyPlenum_", "Use HBRoom or OsZones.\nThis plenumZone will be connect to all thermal zones", GH_ParamAccess.list);
             pManager[0].Optional = true;
-            pManager.AddGenericParameter("IB_ThermalZones", "_ThermalZones", "Add Ironbug_ThermalZones to here to attach their supply and return plenums.", GH_ParamAccess.list);
-            pManager.AddGenericParameter("HoneybeePlenumZone", "returnPlenum_", "Use HBRoom or OsZones.\nThis plenumZone will be connect to all thermal zones", GH_ParamAccess.list);
+            pManager.AddGenericParameter("_ThermalZones", "_ThermalZones", "Add Ironbug_ThermalZones to here to attach their supply and return plenums.", GH_ParamAccess.list);
+            pManager.AddGenericParameter("returnPlenum_", "returnPlenum_", "Use HBRoom or OsZones.\nThis plenumZone will be connect to all thermal zones", GH_ParamAccess.list);
             pManager[2].Optional = true;
      
         }
