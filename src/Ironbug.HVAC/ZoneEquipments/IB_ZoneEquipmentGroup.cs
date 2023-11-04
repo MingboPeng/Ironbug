f5856b7b62d5a4d6c7b95290819c8c1545b27f08
MingboPeng <mingo1214@gmail.com>
2023-11-05 02:41:40 +0800

    fix(ZoneEquipmentGroup): use IIB_ZoneEquipment interface

--- src/Ironbug.HVAC/ZoneEquipments/IB_ZoneEquipmentGroup.cs
+++ src/Ironbug.HVAC/ZoneEquipments/IB_ZoneEquipmentGroup.cs
@@ -1,11 +1,14 @@
 ﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
 using System.Collections.Generic;
 
 namespace Ironbug.HVAC
 {
-    public class IB_ZoneEquipmentGroup
+    public class IB_ZoneEquipmentGroup: IIB_ZoneEquipment
     {
         public List<IB_ZoneEquipment> ZoneEquipments { get; private set; }
+
+        public IB_FieldArgumentSet CustomAttributes => throw new System.NotImplementedException();
 
         public IB_ZoneEquipmentGroup()
         {
@@ -25,6 +28,47 @@ namespace Ironbug.HVAC
         public override string ToString()
         {
             return $"Zone Equipment Group with {this.ZoneEquipments.Count} obj(s) inside";
+        }
+
+
+        public HVACComponent ToOS(Model model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool AddToNode(Node node)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IB_ModelObject Duplicate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetTrackingID()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsInModel(Model model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetFieldValue(IB_Field dataField, object value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetFieldValues(Dictionary<IB_Field, object> DataFields)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string SetTrackingID(string id = null)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
