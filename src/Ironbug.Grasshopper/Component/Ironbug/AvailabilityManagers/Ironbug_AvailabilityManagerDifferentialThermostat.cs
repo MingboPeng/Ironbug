b3709eaa93f3c74bdfd6b6bd5f8c533358b58ea3
MingboPeng <mingo1214@gmail.com>
2023-11-07 16:29:33 +0800

    feat(AvailabilityManager): add AvailabilityManagerDifferentialThermostat

---
+++ src/Ironbug.Grasshopper/Component/Ironbug/AvailabilityManagers/Ironbug_AvailabilityManagerDifferentialThermostat.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Ironbug.HVAC;
+using Ironbug.HVAC.AvailabilityManager;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_AvailabilityManagerDifferentialThermostat : Ironbug_HVACWithParamComponent
+    {
+        public Ironbug_AvailabilityManagerDifferentialThermostat()
+          : base("IB_AvailabilityManagerDifferentialThermostat", "AM_Diff",
+                 "Description",
+                 "Ironbug", "05:SetpointManager & AvailabilityManager", typeof(IB_AvailabilityManagerDifferentialThermostat_FieldSet))
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.secondary;
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Cold Probe", "_coldProbe", $"Add a IB_NodeProbe (cold) to a loop first, and then connect the probe to here for availability manager to use.", GH_ParamAccess.item);
+            pManager.AddGenericParameter("Hot Probe", "_hotProbe", "Add a IB_NodeProbe (hot) to a loop first, and then connect the probe to here for availability manager to use.", GH_ParamAccess.item);
+        }
+
+        
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("AvailabilityManager", "AM", "TODO..", GH_ParamAccess.item);
+        }
+        
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            IB_NodeProbe cold = null;
+            IB_NodeProbe hot = null;
+            if (!DA.GetData(0, ref cold)) return;
+            if (!DA.GetData(0, ref hot)) return;
+
+            var coldID = cold.GetTrackingID();
+            var hotID = hot.GetTrackingID();
+
+            var obj = new IB_AvailabilityManagerDifferentialThermostat();
+            obj.SetSensorNode(coldID, hotID);
+
+            this.SetObjParamsTo(obj);
+            DA.SetData(0, obj);
+
+        }
+
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.AM_Dif;
+
+        public override Guid ComponentGuid => new Guid("F1E8995C-953F-44BF-88DD-076B1F8CBC73");
+    }
+}
---
+++ src/Ironbug.HVAC/AvailabilityManagers/IB_AvailabilityManagerDifferentialThermostat.cs
@@ -0,0 +1,59 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC.AvailabilityManager
+{
+    public class IB_AvailabilityManagerDifferentialThermostat : IB_AvailabilityManager
+    {
+        private string _nodeCID { get => this.Get(string.Empty); set => this.Set(value); }
+        private string _nodeHID { get => this.Get(string.Empty); set => this.Set(value); }
+        public static IB_AvailabilityManagerDifferentialThermostat_FieldSet FieldSet => IB_AvailabilityManagerDifferentialThermostat_FieldSet.Value;
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_AvailabilityManagerDifferentialThermostat();
+
+        private static AvailabilityManagerDifferentialThermostat NewDefaultOpsObj(Model model) => new AvailabilityManagerDifferentialThermostat(model);
+        public IB_AvailabilityManagerDifferentialThermostat() : base(NewDefaultOpsObj(new Model()))
+        {
+        } 
+
+        public void SetSensorNode(string coldProbeTrackingID, string hotProbeTrackingID)
+        {
+            if (string.IsNullOrEmpty(coldProbeTrackingID) || string.IsNullOrEmpty(hotProbeTrackingID))
+                throw new ArgumentException("Invalid probe tracking ID");
+            _nodeCID = coldProbeTrackingID;
+            _nodeHID = hotProbeTrackingID;
+        }
+
+        public override OpenStudio.AvailabilityManager ToOS(Model model)
+        {
+            var obj = base.OnNewOpsObj(NewDefaultOpsObj, model);
+
+            // this will be executed after all loops (nodes) are saved
+            Func<bool> func = () =>
+            {
+                var nodeC = model.GetNodeByTrackingID(_nodeCID);
+                var nodeH = model.GetNodeByTrackingID(_nodeHID);
+                if (nodeC == null || nodeH == null)
+                    return false;
+
+                return obj.setColdNode(nodeC) && obj.setHotNode(nodeH);
+
+            };
+
+            IB_Utility.DelayAddSensorNode(func);
+
+            return obj;
+        }
+
+       
+    }
+
+    public sealed class IB_AvailabilityManagerDifferentialThermostat_FieldSet
+        : IB_FieldSet<IB_AvailabilityManagerDifferentialThermostat_FieldSet, AvailabilityManagerDifferentialThermostat>
+    {
+        private IB_AvailabilityManagerDifferentialThermostat_FieldSet() { }
+
+    }
+
+
+}
