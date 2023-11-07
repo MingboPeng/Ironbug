78f32191a4e43c79b3c5440d656bed0f63c9beea
MingboPeng <mingo1214@gmail.com>
2023-11-07 16:32:27 +0800

    feat(AvailabilityManager): add AvailabilityManagerOptimumStart

---
+++ src/Ironbug.HVAC/AvailabilityManagers/IB_AvailabilityManagerOptimumStart.cs
@@ -0,0 +1,57 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC.AvailabilityManager
+{
+    public class IB_AvailabilityManagerOptimumStart : IB_AvailabilityManager
+    {
+        private string _controlZoneName { get => this.Get(string.Empty); set => this.Set(value); }
+        public static IB_AvailabilityManagerOptimumStart_FieldSet FieldSet => IB_AvailabilityManagerOptimumStart_FieldSet.Value;
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_AvailabilityManagerOptimumStart();
+
+        private static AvailabilityManagerOptimumStart NewDefaultOpsObj(Model model) => new AvailabilityManagerOptimumStart(model);
+        public IB_AvailabilityManagerOptimumStart() : base(NewDefaultOpsObj(new Model()))
+        {
+        } 
+
+        public void SetControlZone(string controlZoneName)
+        {
+            if (string.IsNullOrEmpty(controlZoneName))
+                return;
+            _controlZoneName = controlZoneName;
+        }
+
+        public override OpenStudio.AvailabilityManager ToOS(Model model)
+        {
+            var obj = base.OnNewOpsObj(NewDefaultOpsObj, model);
+            
+            // this will be executed after all loops (nodes) are saved
+            Func<bool> func = () =>
+            {
+                var zone = model.GetThermalZone(_controlZoneName);
+                if (zone == null)
+                    return false;
+
+                return obj.setControlZone(zone);
+
+            };
+
+            IB_Utility.DelayAddSensorNode(func);
+
+
+            return obj;
+        }
+
+        
+    }
+
+    public sealed class IB_AvailabilityManagerOptimumStart_FieldSet
+        : IB_FieldSet<IB_AvailabilityManagerOptimumStart_FieldSet, AvailabilityManagerOptimumStart>
+    {
+        private IB_AvailabilityManagerOptimumStart_FieldSet() { }
+
+    }
+
+
+}
