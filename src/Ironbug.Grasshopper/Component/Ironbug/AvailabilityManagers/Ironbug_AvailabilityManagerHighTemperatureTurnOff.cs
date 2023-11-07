b9de14627ad332bccb2988951e8bed653b91691a
MingboPeng <mingo1214@gmail.com>
2023-11-07 16:30:59 +0800

    feat(AvailabilityManager): add AvailabilityManagerHighTemperatureTurnOff

---
+++ src/Ironbug.Grasshopper/Component/Ironbug/AvailabilityManagers/Ironbug_AvailabilityManagerHighTemperatureTurnOff.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Ironbug.HVAC;
+using Ironbug.HVAC.AvailabilityManager;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_AvailabilityManagerHighTemperatureTurnOff : Ironbug_HVACWithParamComponent
+    {
+        public Ironbug_AvailabilityManagerHighTemperatureTurnOff()
+          : base("IB_AvailabilityManagerHighTemperatureTurnOff", "AM_HighOff",
+                 "Description",
+                 "Ironbug", "05:SetpointManager & AvailabilityManager", typeof(IB_AvailabilityManagerHighTemperatureTurnOff_FieldSet))
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.secondary | GH_Exposure.obscure;
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Sensor Probe", "_probe", "Add a IB_NodeProbe to a loop first, and then connect the probe to here for availability manager to use.", GH_ParamAccess.item);
+        }
+
+        
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("AvailabilityManager", "AM", "TODO..", GH_ParamAccess.item);
+        }
+        
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            IB_NodeProbe probe = null;
+            if (!DA.GetData(0, ref probe)) return;
+
+            var nodeID = probe.GetTrackingID();
+            var obj = new IB_AvailabilityManagerHighTemperatureTurnOff();
+            obj.SetSensorNode(nodeID);
+
+            this.SetObjParamsTo(obj);
+            DA.SetData(0, obj);
+
+        }
+
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.AM_Off;
+
+        public override Guid ComponentGuid => new Guid("3C8F8AEF-426B-43BD-AFDB-397BC8B000FB");
+    }
+}
