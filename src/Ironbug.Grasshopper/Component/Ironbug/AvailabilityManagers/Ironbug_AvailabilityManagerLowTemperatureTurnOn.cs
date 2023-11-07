6378131abd09b6028a24ced9f0d557c15bf3df71
MingboPeng <mingo1214@gmail.com>
2023-11-07 16:32:09 +0800

    feat(AvailabilityManager): add AvailabilityManagerLowTemperatureTurnOn

---
+++ src/Ironbug.Grasshopper/Component/Ironbug/AvailabilityManagers/Ironbug_AvailabilityManagerLowTemperatureTurnOn.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Ironbug.HVAC;
+using Ironbug.HVAC.AvailabilityManager;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_AvailabilityManagerLowTemperatureTurnOn : Ironbug_HVACWithParamComponent
+    {
+        public Ironbug_AvailabilityManagerLowTemperatureTurnOn()
+          : base("IB_AvailabilityManagerLowTemperatureTurnOn", "AM_LowOn",
+                 "Description",
+                 "Ironbug", "05:SetpointManager & AvailabilityManager", typeof(IB_AvailabilityManagerLowTemperatureTurnOn_FieldSet))
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.secondary | GH_Exposure.obscure;
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Sensor Probe", "_probe", "Add a IB_NodeProbe to a loop first, and then connect the probe to here for availability manager to use.", GH_ParamAccess.item);
+        }
+
+        
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("AvailabilityManager", "AM", "TODO..", GH_ParamAccess.item);
+        }
+        
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            IB_NodeProbe probe = null;
+            if (!DA.GetData(0, ref probe)) return;
+
+            var nodeID = probe.GetTrackingID();
+            var obj = new IB_AvailabilityManagerLowTemperatureTurnOn();
+            obj.SetSensorNode(nodeID);
+
+            this.SetObjParamsTo(obj);
+            DA.SetData(0, obj);
+
+        }
+
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.AM_On;
+
+        public override Guid ComponentGuid => new Guid("C9A99554-BF69-48C2-A77A-418319D7DEFA");
+    }
+}
---
+++ src/Ironbug.HVAC/AvailabilityManagers/IB_AvailabilityManagerLowTemperatureTurnOn.cs
@@ -0,0 +1,56 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC.AvailabilityManager
+{
+    public class IB_AvailabilityManagerLowTemperatureTurnOn : IB_AvailabilityManager
+    {
+        private string _nodeID { get => this.Get(string.Empty); set => this.Set(value); }
+        public static IB_AvailabilityManagerLowTemperatureTurnOn_FieldSet FieldSet => IB_AvailabilityManagerLowTemperatureTurnOn_FieldSet.Value;
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_AvailabilityManagerLowTemperatureTurnOn();
+
+        private static AvailabilityManagerLowTemperatureTurnOn NewDefaultOpsObj(Model model) => new AvailabilityManagerLowTemperatureTurnOn(model);
+        public IB_AvailabilityManagerLowTemperatureTurnOn() : base(NewDefaultOpsObj(new Model()))
+        {
+        } 
+
+        public void SetSensorNode(string probeTrackingID)
+        {
+            if (string.IsNullOrEmpty(probeTrackingID))
+                throw new ArgumentException("Invalid probe tracking ID");
+            _nodeID = probeTrackingID;
+        }
+
+        public override OpenStudio.AvailabilityManager ToOS(Model model)
+        {
+            var obj = base.OnNewOpsObj(NewDefaultOpsObj, model);
+
+            // this will be executed after all loops (nodes) are saved
+            Func<bool> func = () =>
+            {
+                var node = model.GetNodeByTrackingID(_nodeID);
+                if (node == null)
+                    return false;
+
+                return obj.setSensorNode(node);
+                
+            };
+
+            IB_Utility.DelayAddSensorNode(func);
+
+            return obj;
+        }
+
+       
+    }
+
+    public sealed class IB_AvailabilityManagerLowTemperatureTurnOn_FieldSet
+        : IB_FieldSet<IB_AvailabilityManagerLowTemperatureTurnOn_FieldSet, AvailabilityManagerLowTemperatureTurnOn>
+    {
+        private IB_AvailabilityManagerLowTemperatureTurnOn_FieldSet() { }
+
+    }
+
+
+}
