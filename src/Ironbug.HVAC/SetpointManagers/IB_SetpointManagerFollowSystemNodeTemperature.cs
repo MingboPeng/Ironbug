70aec0e3bba77c88c0ed9b6949c6a63111f88673
MingboPeng <mingo1214@gmail.com>
2023-11-09 16:33:14 +0800

    feat(SetpointManagerFollowSystemNodeTemperature): new component

---
+++ src/Ironbug.HVAC/SetpointManagers/IB_SetpointManagerFollowSystemNodeTemperature.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+
+namespace Ironbug.HVAC
+{
+    public class IB_SetpointManagerFollowSystemNodeTemperature : IB_SetpointManager
+    {
+        private string _nodeID { get => this.Get(string.Empty); set => this.Set(value); }
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_SetpointManagerFollowSystemNodeTemperature();
+
+        private static SetpointManagerFollowSystemNodeTemperature NewDefaultOpsObj(Model model) => new SetpointManagerFollowSystemNodeTemperature(model);
+
+
+        public IB_SetpointManagerFollowSystemNodeTemperature() : base(NewDefaultOpsObj(new Model()))
+        { 
+        }
+
+
+        public void SetSensorNode(string probeTrackingID)
+        {
+            if (string.IsNullOrEmpty(probeTrackingID))
+                throw new ArgumentException("Invalid probe tracking ID");
+            _nodeID = probeTrackingID;
+        }
+
+        public override HVACComponent ToOS(Model model)
+        {
+            var obj = base.OnNewOpsObj(NewDefaultOpsObj, model);
+
+            // this will be executed after all loops (nodes) are saved
+            Func<bool> func = () =>
+            {
+                var node = model.GetNodeByTrackingID(_nodeID);
+                if (node == null)
+                    return false;
+
+                return obj.setReferenceNode(node);
+
+            };
+
+            IB_Utility.AddDelayFunc(func);
+            return obj;
+        }
+    }
+
+    public sealed class IB_SetpointManagerFollowSystemNodeTemperature_FieldSet
+        : IB_FieldSet<IB_SetpointManagerFollowSystemNodeTemperature_FieldSet, SetpointManagerFollowSystemNodeTemperature>
+    {
+        private IB_SetpointManagerFollowSystemNodeTemperature_FieldSet() { }
+
+    }
+}
