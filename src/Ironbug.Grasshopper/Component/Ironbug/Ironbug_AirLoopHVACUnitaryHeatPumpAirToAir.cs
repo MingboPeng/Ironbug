1e28253f997daea883e8e45c32f8c775be6714ce
MingboPeng <mingo1214@gmail.com>
2023-11-09 16:35:19 +0800

    feat(AirLoopHVACUnitaryHeatPumpAirToAir): add new component

---
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_AirLoopHVACUnitaryHeatPumpAirToAir.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Ironbug.HVAC;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_AirLoopHVACUnitaryHeatPumpAirToAir : Ironbug_HVACWithParamComponent
+    {
+        public Ironbug_AirLoopHVACUnitaryHeatPumpAirToAir()
+          : base("IB_AirLoopHVACUnitaryHeatPumpAirToAir", "HeatPumpAirToAir",
+              "Description",
+              "Ironbug", "02:LoopComponents",
+              typeof(IB_AirLoopHVACUnitaryHeatPumpAirToAir_FieldSet))
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary | GH_Exposure.obscure;
+
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.UnitaryHP;
+
+        public override Guid ComponentGuid => new Guid("13B7C0BF-735A-4B85-9C48-DC3571A84934");
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("HeatingCoil", "_coilH_", $"{EPDoc.AirLoopHVACUnitaryHeatPumpAirToAir.Field_HeatingCoilName} \n\n{EPDoc.AirLoopHVACUnitaryHeatPumpAirToAir.Field_HeatingCoilObjectType}", GH_ParamAccess.item);
+            pManager[0].Optional = true;
+            pManager.AddGenericParameter("CoolingCoil", "_coilC_", $"{EPDoc.AirLoopHVACUnitaryHeatPumpAirToAir.Field_CoolingCoilName} \n\n{EPDoc.AirLoopHVACUnitaryHeatPumpAirToAir.Field_CoolingCoilObjectType}", GH_ParamAccess.item);
+            pManager[1].Optional = true;
+            pManager.AddGenericParameter("Fan", "_fan_", $"{EPDoc.AirLoopHVACUnitaryHeatPumpAirToAir.Field_SupplyAirFanName} \n\n{EPDoc.AirLoopHVACUnitaryHeatPumpAirToAir.Field_SupplyAirFanObjectType}", GH_ParamAccess.item);
+            pManager[2].Optional = true;
+            pManager.AddGenericParameter("SupplementalHeatingCoil", "spCoilH_", $"{EPDoc.AirLoopHVACUnitaryHeatPumpAirToAir.Field_SupplementalHeatingCoilName} \n\n{EPDoc.AirLoopHVACUnitaryHeatPumpAirToAir.Field_SupplementalHeatingCoilObjectType}", GH_ParamAccess.item); 
+            pManager[3].Optional = true;
+
+            pManager.AddGenericParameter("ControllingZone", "ctrlZone_", "The controlling zone for thermostat location. It is required to set a valid when the unitary system is used within an air loop.", GH_ParamAccess.item);
+            pManager[4].Optional = true;
+
+        }
+        
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("UnitarySystem", "UniSys", "Connect to airloop's supply side", GH_ParamAccess.item);
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            HVAC.BaseClass.IB_Fan fan = null;
+            HVAC.BaseClass.IB_CoilDX coilH = null;
+            HVAC.BaseClass.IB_CoilDX coilC = null;
+            HVAC.BaseClass.IB_CoilHeatingBasic spCoilH = null;
+            HVAC.BaseClass.IB_ThermalZone zone = null;
+
+            var obj = new HVAC.IB_AirLoopHVACUnitaryHeatPumpAirToAir();
+            if (DA.GetData(0, ref coilH)) obj.SetHeatingCoil(coilH);
+            if (DA.GetData(1, ref coilC)) obj.SetCoolingCoil(coilC);
+            if (DA.GetData(2, ref fan)) obj.SetFan(fan);
+            if (DA.GetData(3, ref spCoilH)) obj.SetSupplementalHeatingCoil(spCoilH);
+
+            if (DA.GetData(4, ref zone)) obj.SetControllingZone(zone);
+            
+            this.SetObjParamsTo(obj);
+            DA.SetData(0, obj);
+        }
+        
+    }
+}
---
+++ src/Ironbug.HVAC/LoopObjs/IB_AirLoopHVACUnitaryHeatPumpAirToAir.cs
@@ -0,0 +1,103 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC
+{
+    public class IB_AirLoopHVACUnitaryHeatPumpAirToAir : IB_AirLoopHVACUnitary
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () 
+            => new IB_AirLoopHVACUnitaryHeatPumpAirToAir();
+
+        private static AirLoopHVACUnitaryHeatPumpAirToAir NewDefaultOpsObj(Model m)
+        {
+            var sch = new ScheduleRuleset(m);
+            var fan = new FanConstantVolume(m);
+            var cH = new CoilHeatingDXSingleSpeed(m);
+            var cC = new CoilCoolingDXSingleSpeed(m);
+            var sCH = new CoilHeatingElectric(m);
+            var obj = new AirLoopHVACUnitaryHeatPumpAirToAir(m, sch, fan, cH, cC, sCH);
+            return obj;
+        }
+
+        private IB_CoilDX _coolingCoil => this.GetChild<IB_CoilDX>(0);
+        private IB_CoilDX _heatingCoil => this.GetChild<IB_CoilDX>(1); 
+        private IB_Fan _fan => this.GetChild<IB_Fan>(2);
+        private IB_CoilHeatingBasic _supplementalHeatingCoil => this.GetChild<IB_CoilHeatingBasic>(3);
+        private string _controlZoneName { get => this.Get(string.Empty); set => this.Set(value); }
+
+        public IB_AirLoopHVACUnitaryHeatPumpAirToAir() 
+            : base(NewDefaultOpsObj(new Model()))
+        {
+            this.AddChild(new IB_CoilCoolingDXSingleSpeed());
+            this.AddChild(new IB_CoilHeatingDXSingleSpeed());
+            this.AddChild(new IB_FanConstantVolume());
+            this.AddChild(new IB_CoilHeatingElectric());
+        }
+
+
+        public void SetCoolingCoil(IB_CoilDX coolingCoil)
+        {
+            this.SetChild(0, coolingCoil);
+        }
+
+        public void SetHeatingCoil(IB_CoilDX heatingCoil)
+        {
+            this.SetChild(1, heatingCoil);
+        }
+
+        public void SetFan(IB_Fan Fan)
+        {
+            this.SetChild(2, Fan);
+        }
+
+        public void SetSupplementalHeatingCoil(IB_CoilHeatingBasic heatingCoil)
+        {
+            this.SetChild(3, heatingCoil);
+        }
+
+        public void SetControlZone(string controlZoneName)
+        {
+            if (string.IsNullOrEmpty(controlZoneName))
+                throw new ArgumentException("Invalid control zone");
+            _controlZoneName = controlZoneName;
+        }
+
+        public override HVACComponent ToOS(Model model)
+        {
+            var obj = base.OnNewOpsObj(NewDefaultOpsObj, model);
+
+            if (this._coolingCoil != null) obj.setCoolingCoil(this._coolingCoil.ToOS(model));
+            if (this._heatingCoil != null) obj.setHeatingCoil(this._heatingCoil.ToOS(model));
+            if (this._fan != null) obj.setSupplyAirFan(this._fan.ToOS(model));
+            if (this._supplementalHeatingCoil != null) obj.setSupplementalHeatingCoil(this._supplementalHeatingCoil.ToOS(model));
+
+           
+            if (!string.IsNullOrEmpty(_controlZoneName))
+            {
+                // this will be executed after all loops (nodes) are saved
+                Func<bool> func = () =>
+                {
+                    var zone = model.GetThermalZone(_controlZoneName);
+                    if (zone == null)
+                        return false;
+
+                    return obj.setControllingZone(zone);
+
+                };
+
+                IB_Utility.AddDelayFunc(func);
+            }
+
+            return obj;
+
+        }
+    }
+
+    public sealed class IB_AirLoopHVACUnitaryHeatPumpAirToAir_FieldSet
+        : IB_FieldSet<IB_AirLoopHVACUnitaryHeatPumpAirToAir_FieldSet, AirLoopHVACUnitaryHeatPumpAirToAir>
+    {
+        private IB_AirLoopHVACUnitaryHeatPumpAirToAir_FieldSet() {}
+
+    }
+}
