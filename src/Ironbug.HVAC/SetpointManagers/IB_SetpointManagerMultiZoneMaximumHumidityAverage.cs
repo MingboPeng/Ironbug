5e113616043ce0ff8b6ded1c90fdfd2ed7c38889
MingboPeng <mingo1214@gmail.com>
2023-11-10 11:50:04 +0800

    feat(SetpointManagerMultiZoneMinimumHumidityAverage): new components

---
+++ src/Ironbug.HVAC/SetpointManagers/IB_SetpointManagerMultiZoneMaximumHumidityAverage.cs
@@ -0,0 +1,35 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC
+{
+    public class IB_SetpointManagerMultiZoneMaximumHumidityAverage : IB_SetpointManager
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_SetpointManagerMultiZoneMaximumHumidityAverage();
+
+        private static SetpointManagerMultiZoneMaximumHumidityAverage NewDefaultOpsObj(Model model)
+            => new SetpointManagerMultiZoneMaximumHumidityAverage(model);
+
+        public IB_SetpointManagerMultiZoneMaximumHumidityAverage() : base(NewDefaultOpsObj(new Model()))
+        {
+        }
+
+        public override HVACComponent ToOS(Model model)
+        {
+            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+        }
+    }
+    public sealed class IB_SetpointManagerMultiZoneMaximumHumidityAverage_FieldSet
+        : IB_FieldSet<IB_SetpointManagerMultiZoneMaximumHumidityAverage_FieldSet, SetpointManagerMultiZoneMaximumHumidityAverage>
+    {
+        private IB_SetpointManagerMultiZoneMaximumHumidityAverage_FieldSet() { }
+
+        public IB_Field MaximumSetpointHumidityRatio { get; }
+            = new IB_TopField("MaximumSetpointHumidityRatio", "Max");
+        public IB_Field MinimumSetpointHumidityRatio { get; }
+            = new IB_TopField("MinimumSetpointHumidityRatio", "Min");
+
+
+    }
+}
---
+++ src/Ironbug.HVAC/SetpointManagers/IB_SetpointManagerMultiZoneMinimumHumidityAverage.cs
@@ -0,0 +1,35 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC
+{
+    public class IB_SetpointManagerMultiZoneMinimumHumidityAverage : IB_SetpointManager
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_SetpointManagerMultiZoneMinimumHumidityAverage();
+
+        private static SetpointManagerMultiZoneMinimumHumidityAverage NewDefaultOpsObj(Model model)
+            => new SetpointManagerMultiZoneMinimumHumidityAverage(model);
+
+        public IB_SetpointManagerMultiZoneMinimumHumidityAverage() : base(NewDefaultOpsObj(new Model()))
+        {
+        }
+
+        public override HVACComponent ToOS(Model model)
+        {
+            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+        }
+    }
+    public sealed class IB_SetpointManagerMultiZoneMinimumHumidityAverage_FieldSet
+        : IB_FieldSet<IB_SetpointManagerMultiZoneMinimumHumidityAverage_FieldSet, SetpointManagerMultiZoneMinimumHumidityAverage>
+    {
+        private IB_SetpointManagerMultiZoneMinimumHumidityAverage_FieldSet() { }
+
+        public IB_Field MaximumSetpointHumidityRatio { get; }
+            = new IB_TopField("MaximumSetpointHumidityRatio", "Max");
+        public IB_Field MinimumSetpointHumidityRatio { get; }
+            = new IB_TopField("MinimumSetpointHumidityRatio", "Min");
+
+
+    }
+}
