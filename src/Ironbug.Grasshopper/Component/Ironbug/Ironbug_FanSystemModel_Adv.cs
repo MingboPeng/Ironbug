5085286805dfb20cc689c9fb73a11994ce1696ae
MingboPeng <mingo1214@gmail.com>
2023-11-22 21:48:34 +0800

    fix(FanSystemModel): add adv mode with speeds input

---
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_FanSystemModel_Adv.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using Ironbug.Grasshopper.Properties;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_FanSystemModel_Adv : Ironbug_DuplicableHVACWithParamComponent
+    {
+        
+        public Ironbug_FanSystemModel_Adv()
+          : base("IB_FanSystemModel+", "FanSysModel+",
+              "Description",
+              "Ironbug", "02:LoopComponents",
+              typeof(HVAC.IB_FanSystemModel_FieldSet))
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.tertiary;
+
+        
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("speeds", "speeds", EPDoc.FanSystemModel.Field_SetSpeedFlowFractionSpeedElectricPowerFraction + "\n\nExample:\n0.33,0.12\n0.66,0.51\n1.0,1.0", GH_ParamAccess.list);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("FanSystemModel", "Fan", "", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var spds = new List<string>();
+            DA.GetDataList(0, spds);
+
+            var obj = new HVAC.IB_FanSystemModel();
+            obj.SetSpeeds(spds);
+
+            this.SetObjParamsTo(obj);
+            var objs = this.SetObjDupParamsTo(obj);
+            DA.SetDataList(0, objs);
+        }
+
+        protected override System.Drawing.Bitmap Icon => Resources.FanSysModel;
+
+        public override Guid ComponentGuid => new Guid("{5140633A-F350-4235-828A-5DD0670E4040}");
+
+
+    }
+
+   
+}
--- src/Ironbug.HVAC/LoopObjs/IB_FanSystemModel.cs
+++ src/Ironbug.HVAC/LoopObjs/IB_FanSystemModel.cs
@@ -1,6 +1,8 @@
 ﻿using Ironbug.HVAC.BaseClass;
 using OpenStudio;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ironbug.HVAC
 {
@@ -10,13 +12,57 @@ namespace Ironbug.HVAC
         
         private static FanSystemModel NewDefaultOpsObj(Model model) => new FanSystemModel(model);
 
+        //odd: double flowFraction
+        //even: double electricPowerFraction
+        public List<double> Speeds
+        {
+            get => this.TryGetList<double>();
+            private set => this.Set(value);
+        }
+
         public IB_FanSystemModel():base(NewDefaultOpsObj(new Model()))
         {
         }
-        
+
+        public void SetSpeeds(List<double> speeds)
+        {
+            this.Speeds = speeds;
+        }
+
+        /// <summary>
+        /// flowFraction and electricPowerFraction pair and separated by ","
+        /// </summary>
+        /// <param name="speeds"></param>
+        public void SetSpeeds(List<string> speeds)
+        {
+            var sp = new List<double>();
+            foreach (string s in speeds)
+            {
+                var p = s.Split(',').Select(n => double.Parse(n));
+                if (p == null || p.Count() != 2)
+                    throw new ArgumentException($"Invalid speed pair: {s}");
+                if (p.Any(_=>_>1 || _<0))
+                    throw new ArgumentException($"Invalid fraction value (0-1): {s}");
+                sp.AddRange(p);
+
+            }
+            this.Speeds = sp;
+        }
+
         public override HVACComponent ToOS(Model model)
         {
-            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+            var obj = base.OnNewOpsObj(NewDefaultOpsObj, model);
+            if (Speeds!= null && Speeds.Any())
+            {
+                var flows = Speeds.Where((_, i) => i % 2 != 0);
+                var powers = Speeds.Where((_, i) => i % 2 == 0);
+                var speeds = flows.Zip(powers, (f, p) => new FanSystemModelSpeed(f, p));
+                foreach (var speed in speeds)
+                {
+                    obj.addSpeed(speed);
+                }
+            }
+            return obj;
         }
         
         
