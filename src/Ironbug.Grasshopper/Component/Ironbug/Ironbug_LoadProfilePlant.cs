97e763d3caadd14136874e1dfd14a919203ca0ad
MingboPeng <mingo1214@gmail.com>
2024-02-07 22:41:34 +0800

    feat(LoadProfilePlant): add LoadProfilePlant

---
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_LoadProfilePlant.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Grasshopper.Kernel;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_LoadProfilePlant : Ironbug_DuplicableHVACWithParamComponent
+    {
+        public Ironbug_LoadProfilePlant()
+          : base("IB_LoadProfilePlant", "LoadProfile",
+              "Description",
+              "Ironbug", "02:LoopComponents",
+              typeof(HVAC.IB_LoadProfilePlant_FieldSet))
+        {
+            
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.septenary;
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("LoadProfilePlant", "Load", "LoadProfilePlant as a plant demand component", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var obj = new HVAC.IB_LoadProfilePlant();
+
+            this.SetObjParamsTo(obj);
+            var objs = this.SetObjDupParamsTo(obj);
+            DA.SetDataList(0, objs);
+        }
+
+        protected override System.Drawing.Bitmap Icon => null;
+
+        public override Guid ComponentGuid => new Guid("{CD1CC589-18FC-4DA8-8509-0D6761C3B4EA}");
+
+
+    }
+
+   
+}
---
+++ src/Ironbug.HVAC/LoopObjs/IB_LoadProfilePlant.cs
@@ -0,0 +1,29 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC
+{
+    public class IB_LoadProfilePlant : IB_HVACObject, IIB_PlantLoopObjects
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_LoadProfilePlant();
+        private static LoadProfilePlant NewDefaultOpsObj(Model model)
+            => new LoadProfilePlant(model);
+        
+
+        public IB_LoadProfilePlant():base(NewDefaultOpsObj(new Model()))
+        {
+        }
+        public override HVACComponent ToOS(Model model)
+        {
+            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+        }
+        
+    }
+
+    public sealed class IB_LoadProfilePlant_FieldSet
+        : IB_FieldSet<IB_LoadProfilePlant_FieldSet, LoadProfilePlant>
+    {
+        private IB_LoadProfilePlant_FieldSet() { }
+    }
+}
