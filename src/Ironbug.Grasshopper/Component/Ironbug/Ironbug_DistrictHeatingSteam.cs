134f6311c96991989c8253309c67cdd0e165627e
MingboPeng <mingo1214@gmail.com>
2024-03-01 21:21:16 +0000

    feat(DistrictHeatingSteam): add new DistrictHeatingSteam

---
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_DistrictHeatingSteam.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Grasshopper.Kernel;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_DistrictHeatingSteam : Ironbug_DuplicableHVACWithParamComponent
+    {
+        
+        public Ironbug_DistrictHeatingSteam()
+          : base("IB_DistrictHeatingSteam", "DistrictHeatingSteam",
+              "Description",
+              "Ironbug", "02:LoopComponents",
+              typeof(HVAC.IB_DistrictHeatingSteam_FieldSet))
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.quarternary;
+        
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("DistrictHeating", "DistHeating", "DistrictHeating for plant loop's supply.", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var obj = new HVAC.IB_DistrictHeatingSteam();
+
+            this.SetObjParamsTo(obj);
+            var objs = this.SetObjDupParamsTo(obj);
+            DA.SetDataList(0, objs);
+        }
+
+
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.DistricHeating;
+
+
+        public override Guid ComponentGuid => new Guid("{7C8AB1BC-7E49-4CD2-A4D4-EBBF5CE27B13}");
+    }
+}
---
+++ src/Ironbug.HVAC/LoopObjs/IB_DistrictHeatingWater.cs
@@ -0,0 +1,34 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC
+{
+
+    public class IB_DistrictHeatingWater : IB_HVACObject, IIB_PlantLoopObjects
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_DistrictHeatingWater();
+
+        private static DistrictHeatingWater NewDefaultOpsObj(Model model) => new DistrictHeatingWater(model);
+        public IB_DistrictHeatingWater() : base(NewDefaultOpsObj(new Model()))
+        {
+        }
+
+        public override HVACComponent ToOS(Model model)
+        {
+            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+        }
+    }
+
+    public sealed class IB_DistrictHeatingWater_FieldSet
+        : IB_FieldSet<IB_DistrictHeatingWater_FieldSet, DistrictHeatingWater>
+    {
+        private IB_DistrictHeatingWater_FieldSet() { }
+        
+        public IB_Field Name { get; }
+            = new IB_BasicField("Name", "Name");
+        public IB_Field NominalCapacity { get; }
+            = new IB_BasicField("NominalCapacity", "Capacity");
+
+    }
+}
