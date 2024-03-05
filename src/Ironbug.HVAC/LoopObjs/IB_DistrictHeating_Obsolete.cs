3d1a6e40d0de425229be5f19cb072833d7125e70
Mingbo Peng <mingo1214@gmail.com>
2024-03-05 15:12:37 -0500

    fix(IB_DistrictHeating): add IB_DistrictHeating_Obsolete to support old files

---
+++ src/Ironbug.HVAC/LoopObjs/IB_DistrictHeating_Obsolete.cs
@@ -0,0 +1,29 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC
+{
+    [Obsolete("Use IB_DistrictHeatingWater instead.", true)]
+    public class IB_DistrictHeating : IB_HVACObject, IIB_PlantLoopObjects
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_DistrictHeating();
+
+        private static DistrictHeatingWater NewDefaultOpsObj(Model model) => new DistrictHeatingWater(model);
+        public IB_DistrictHeating() : base(NewDefaultOpsObj(new Model()))
+        {
+        }
+
+        public override HVACComponent ToOS(Model model)
+        {
+            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+        }
+    }
+
+    public sealed class IB_DistrictHeating_FieldSet
+        : IB_FieldSet<IB_DistrictHeating_FieldSet, DistrictHeatingWater>
+    {
+        private IB_DistrictHeating_FieldSet() { }
+        
+    }
+}
