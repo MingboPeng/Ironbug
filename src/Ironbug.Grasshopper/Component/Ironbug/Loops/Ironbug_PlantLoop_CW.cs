2f76e345386b87f5ca8545c529d0b9e7a72d9432
Mingbo Peng <mingo1214@gmail.com>
2024-10-09 19:02:13 +0800

    fix(naming): CHW for chilled water, CW for condenser water

--- src/Ironbug.Grasshopper/Component/Ironbug/Loops/Ironbug_PlantLoop_CW.cs
+++ src/Ironbug.Grasshopper/Component/Ironbug/Loops/Ironbug_PlantLoop_CW.cs
@@ -35,7 +35,7 @@ namespace Ironbug.Grasshopper.Component
         
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddGenericParameter("ChilledWaterPlantLoop", "cwLoop", "ChilledWaterPlantLoop", GH_ParamAccess.item);
+            pManager.AddGenericParameter("ChilledWaterPlantLoop", "CHWLoop", "ChilledWaterPlantLoop", GH_ParamAccess.item);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
