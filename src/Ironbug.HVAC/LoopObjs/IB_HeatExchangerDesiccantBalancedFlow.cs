07880f62fec5f0af1c52688fd6f071c6db8308e3
Mingbo Peng <mingo1214@gmail.com>
2024-10-09 17:45:27 +0800

    feat(HeatExchangerDesiccantBalancedFlow): support HeatExchangerDesiccantBalancedFlow

---
+++ src/Ironbug.HVAC/LoopObjs/IB_HeatExchangerDesiccantBalancedFlow.cs
@@ -0,0 +1,30 @@
+﻿using Ironbug.HVAC.BaseClass;
+using OpenStudio;
+using System;
+
+namespace Ironbug.HVAC
+{
+    public class IB_HeatExchangerDesiccantBalancedFlow : IB_HVACObject, IIB_AirLoopObject
+    {
+        protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_HeatExchangerDesiccantBalancedFlow();
+
+        private static HeatExchangerDesiccantBalancedFlow NewDefaultOpsObj(Model model) => new HeatExchangerDesiccantBalancedFlow(model);
+
+        public IB_HeatExchangerDesiccantBalancedFlow() : base(NewDefaultOpsObj(new Model()))
+        {
+
+        }
+
+
+        public override HVACComponent ToOS(Model model)
+        {
+            return base.OnNewOpsObj(NewDefaultOpsObj, model);
+        }
+    }
+
+    public sealed class IB_HeatExchangerDesiccantBalancedFlow_FieldSet
+        : IB_FieldSet<IB_HeatExchangerDesiccantBalancedFlow_FieldSet, HeatExchangerDesiccantBalancedFlow>
+    {
+        private IB_HeatExchangerDesiccantBalancedFlow_FieldSet() { }
+    }
+}
