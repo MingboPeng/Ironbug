f88fbfd935c62fd7d7ae5ce9ea73f4f070eede4c
Mingbo Peng <mingo1214@gmail.com>
2025-07-29 08:28:12 -0400

    fix(doc): fix ASHP output param descriptions

--- src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_HeatPumpPlantLoopEIRHeating_AirSource.cs
+++ src/Ironbug.Grasshopper/Component/Ironbug/Ironbug_HeatPumpPlantLoopEIRHeating_AirSource.cs
@@ -15,7 +15,7 @@ namespace Ironbug.Grasshopper.Component
         }
 
         public override GH_Exposure Exposure => GH_Exposure.quarternary;
-        
+
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddGenericParameter("CompanionCoolingHeatPump", "CompanionClnHP", "CompanionCoolingHeatPump", GH_ParamAccess.item);
@@ -24,10 +24,10 @@ namespace Ironbug.Grasshopper.Component
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddGenericParameter("HeatPumpPlantLoopEIRHeating", "HtnHP", "Connect to chilled water loop's supply side.", GH_ParamAccess.item);
+            pManager.AddGenericParameter("HeatPumpPlantLoopEIRHeating", "HtnHP", "Connect to hot water loop's supply side.", GH_ParamAccess.item);
         }
 
-        
+
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             IB_HeatPumpPlantLoopEIRCooling hp = null;
