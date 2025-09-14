8cd315c40bbf0559eb30299180d36855099884ec
Mingbo Peng <mingo1214@gmail.com>
2025-09-13 23:28:44 -0400

    fix(HVAC): remove tuple in Vertical HX and PlantEquipmentOperationScheme for net48
    - Add GFuncItem class to IB_GroundHeatExchangerVertical for better G-function handling
    - Implement IEquatable interface for GFuncItem and PlantEquipmentOperationSchemeItem
    - Replace tuple with custom class in IB_PlantEquipmentOperationSchemeBase
    - Update related methods to use new class structures

--- src/Ironbug.HVAC/LoopObjs/IB_GroundHeatExchangerVertical.cs
+++ src/Ironbug.HVAC/LoopObjs/IB_GroundHeatExchangerVertical.cs
@@ -8,23 +8,52 @@ namespace Ironbug.HVAC
 {
     public class IB_GroundHeatExchangerVertical : IB_HVACObject, IIB_PlantLoopObjects
     {
+        public class GFuncItem : IEquatable<GFuncItem>
+        {
+            public double Ln { get; set; }
+            public double GValue { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                return Equals(obj as GFuncItem);
+            }
+
+            public bool Equals(GFuncItem other)
+            {
+                return other != null &&
+                       Ln == other.Ln &&
+                       GValue == other.GValue;
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked // Overflow is fine, just wrap
+                {
+                    int hash = 17;
+                    hash = hash * 23 + Ln.GetHashCode();
+                    hash = hash * 23 + GValue.GetHashCode();
+                    return hash;
+                }
+            }
+        }
+
         protected override Func<IB_ModelObject> IB_InitSelf => () => new IB_GroundHeatExchangerVertical();
         private static GroundHeatExchangerVertical NewDefaultOpsObj(Model model) => new GroundHeatExchangerVertical(model);
 
-        public List<(double Ln, double GValue)> GFuncs 
+        public List<GFuncItem> GFuncs
         {
-            get => TryGetList<(double Ln, double GValue)>();
-            private set => Set(value); 
-        } 
+            get => TryGetList<GFuncItem>();
+            private set => Set(value);
+        }
 
-        public IB_GroundHeatExchangerVertical():base(NewDefaultOpsObj)
+        public IB_GroundHeatExchangerVertical() : base(NewDefaultOpsObj)
         {
         }
 
         public void AddGFuncs(List<string> GFuncStrings)
         {
             var cleanedV = GFuncStrings.Select(_ => GetValue(_)).ToList();
-            if (cleanedV.Count %2 != 0)
+            if (cleanedV.Count % 2 != 0)
             {
                 throw new ArgumentException("Input list is not even count. The odd item has to be gFunctionLN, and its value the next item");
             }
@@ -36,7 +65,7 @@ namespace Ironbug.HVAC
             {
                 var ln = cleanedV[i * 2];
                 var gV = cleanedV[i * 2 + 1];
-                this.GFuncs.Add((ln, gV));
+                this.GFuncs.Add(new GFuncItem { Ln = ln, GValue = gV });
                 obj.addGFunction(ln, gV);
             }
 
@@ -54,7 +83,7 @@ namespace Ironbug.HVAC
             var gf = this.GFuncs;
             foreach (var item in gf)
             {
-                obj.addGFunction(item.Ln,item.GValue);
+                obj.addGFunction(item.Ln, item.GValue);
             }
             return obj;
         }
--- src/Ironbug.HVAC/LoopObjs/IB_PlantEquipmentOperationSchemeBase.cs
+++ src/Ironbug.HVAC/LoopObjs/IB_PlantEquipmentOperationSchemeBase.cs
@@ -7,19 +7,50 @@ namespace Ironbug.HVAC
 {
     public abstract class IB_PlantEquipmentOperationSchemeBase : IB_ModelObject
     {
-        
+        public class PlantEquipmentOperationSchemeItem : IEquatable<PlantEquipmentOperationSchemeItem>
+        {
+            public int Limit { get; set; }
+            public IB_HVACObject Obj { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                return Equals(obj as PlantEquipmentOperationSchemeItem);
+            }
+
+            public bool Equals(PlantEquipmentOperationSchemeItem other)
+            {
+                return other != null &&
+                       Limit == other.Limit &&
+                       Obj == other.Obj;
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked // Overflow is fine, just wrap
+                {
+                    int hash = 17;
+                    hash = hash * 23 + Limit.GetHashCode();
+                    hash = hash * 23 + Obj.GetHashCode();
+                    return hash;
+                }
+            }
+
+
+        }
+
         public IB_PlantEquipmentOperationSchemeBase(Func<OpenStudio.Model, ModelObject> ghostObjInit) : base(ghostObjInit)
         {
         }
-        protected List<(int limit, IB_HVACObject obj)> _equipments 
-        { 
-            get => TryGetList<(int limit, IB_HVACObject obj)>(); 
-            set => Set(value); 
+
+        protected List<PlantEquipmentOperationSchemeItem> _equipments
+        {
+            get => TryGetList<PlantEquipmentOperationSchemeItem>();
+            set => Set(value);
         }
 
         public void AddEquipment(int limit, IB_HVACObject obj)
         {
-            this._equipments.Add((limit, obj));
+            this._equipments.Add(new PlantEquipmentOperationSchemeItem { Limit = limit, Obj = obj });
         }
 
         public abstract ModelObject ToOS(Model model, PlantLoop loop);
