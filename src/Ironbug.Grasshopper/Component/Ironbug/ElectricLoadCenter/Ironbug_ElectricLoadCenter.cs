eaddf20b72f60c3003be683b61a28f59c8628602
Mingbo Peng <mingo1214@gmail.com>
2025-10-04 17:07:44 -0400

    feat(ElectricLoadCenter): add ElectricLoadCenter components and update saving functionality
    - Add Ironbug_ElectricLoadCenter component for main panel of ElectricLoadCenter
    - Extend Ironbug_ElectricLoadCenterDistribution for sub-panel components
    - Update Ironbug_SaveOSModel to include ElectricLoadCenter saving functionality
    - Obsolete Ironbug_SaveOSModel_Obsolete3 due to updated saving process

---
+++ src/Ironbug.Grasshopper/Component/Ironbug/ElectricLoadCenter/Ironbug_ElectricLoadCenter.cs
@@ -0,0 +1,66 @@
+﻿using Grasshopper.Kernel;
+using Ironbug.Grasshopper.Properties;
+using Ironbug.HVAC;
+using System;
+using System.Collections.Generic;
+
+namespace Ironbug.Grasshopper.Component
+{
+    public class Ironbug_ElectricLoadCenter : Ironbug_Component
+    {
+        public Ironbug_ElectricLoadCenter()
+          : base("IB_ElectricLoadCenter", "ElectricLoadCenter",
+              "The main panel of the ElectricLoadCenter",
+              "Ironbug", "08:ElectricLoadCenter"
+              )
+        {
+        }
+
+        //public override GH_Exposure Exposure => GH_Exposure.septenary;
+
+        
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("subPanels", "subPanels", "A list of ElectricLoadCenters", GH_ParamAccess.list);
+            pManager.AddGenericParameter("_powerInTransformer", "_powerInTransformer", "An optional input for a transformer for transferring electricity from the grid to a building (as distribution transformers) when applicable", GH_ParamAccess.item);
+            pManager.AddGenericParameter("_powerOutTransformer", "_powerOutTransformer", "An optional input for a transformer for transferring electricity from onsite generators to the grid when applicable", GH_ParamAccess.item);
+
+            pManager[0].DataMapping = GH_DataMapping.Flatten;
+            pManager[1].Optional = true;
+            pManager[2].Optional = true;
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("ElectricLoadCenter", "ELC", "ElectricLoadCenter main panel", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var obj = new HVAC.IB_ElectricLoadCenter();
+
+
+            var subPanels = new List<HVAC.IB_ElectricLoadCenterDistribution>();
+            IB_ElectricLoadCenterTransformer inTransformer = null;
+            IB_ElectricLoadCenterTransformer outTransformer = null;
+
+            DA.GetDataList(0, subPanels);
+            DA.GetData(1, ref inTransformer);
+            DA.GetData(2, ref outTransformer);
+
+            obj.SetSubPanels(subPanels);
+            obj.SetPowerInTransformer(inTransformer);
+            obj.SetPowerOutTransformer(outTransformer);
+            
+            DA.SetData(0, obj);
+        }
+
+        protected override System.Drawing.Bitmap Icon => null;
+
+        public override Guid ComponentGuid => new Guid("EA1FF6A4-A434-4DF0-8885-8C1699D92C02");
+
+
+    }
+
+   
+}
--- src/Ironbug.Grasshopper/Component/Ironbug/ElectricLoadCenter/Ironbug_ElectricLoadCenterDistribution.cs
+++ src/Ironbug.Grasshopper/Component/Ironbug/ElectricLoadCenter/Ironbug_ElectricLoadCenterDistribution.cs
@@ -1,35 +1,75 @@
-﻿using System;
-using Grasshopper.Kernel;
+﻿using Grasshopper.Kernel;
 using Ironbug.Grasshopper.Properties;
+using Ironbug.HVAC;
+using Ironbug.HVAC.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ironbug.Grasshopper.Component
 {
     public class Ironbug_ElectricLoadCenterDistribution : Ironbug_HVACWithParamComponent
     {
         public Ironbug_ElectricLoadCenterDistribution()
-          : base("IB_ElectricLoadCenterDistribution", "ElectricLoadCenterDistribution",
+          : base("IB_ElectricLoadCenterDistribution", "SubPanel",
               "Description",
               "Ironbug", "08:ElectricLoadCenter",
               typeof(HVAC.IB_ElectricLoadCenterDistribution_FieldSet))
         {
-            
+
         }
 
         //public override GH_Exposure Exposure => GH_Exposure.septenary;
 
-        
+
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddGenericParameter("_generators", "_generators", "A list of IB_Generator", GH_ParamAccess.list);
+            pManager.AddGenericParameter("_inverter", "_inverter", "", GH_ParamAccess.item);
+            pManager.AddGenericParameter("_transformer", "_transformer", "", GH_ParamAccess.item);
+            pManager.AddGenericParameter("_electricalStorage", "_electricalStorage", "", GH_ParamAccess.item);
+            pManager.AddGenericParameter("_storageConverter", "_storageConverter", "", GH_ParamAccess.item);
+
+            pManager[0].Optional = true;
+            pManager[0].DataMapping = GH_DataMapping.Flatten;
+            pManager[1].Optional = true;
+            pManager[2].Optional = true;
+            pManager[3].Optional = true;
+            pManager[4].Optional = true;
         }
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddGenericParameter("Distribution", "Distribution", "Distribution", GH_ParamAccess.item);
+            pManager.AddGenericParameter("subPanel", "subPanel", "A distribution sub-panel of the ElectricLoadCenter main panel", GH_ParamAccess.item);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             var obj = new HVAC.IB_ElectricLoadCenterDistribution();
+
+            var generators = new List<HVAC.BaseClass.IB_Generator>();
+            IB_ElecInverter inverter = null;
+            IB_ElectricLoadCenterTransformer transformer = null;
+            IB_ElecStorage storage = null;
+            IB_ElectricLoadCenterStorageConverter converter = null;
+
+            DA.GetDataList(0, generators);
+            DA.GetData(1, ref inverter);
+            DA.GetData(2, ref transformer);
+            DA.GetData(3, ref storage);
+            DA.GetData(4, ref converter);
+
+            if (generators != null && generators.Any()) 
+                obj.SetGenerators(generators);
+            if (inverter != null)
+                obj.SetInverter(inverter);
+            if (transformer != null)
+                obj.SetTransformer(transformer);
+            if (storage != null)
+                obj.SetElectricalStorage(storage);
+            if (converter != null)
+                obj.SetStorageConverter(converter);
+
 
             this.SetObjParamsTo(obj);
             var objs = this.SetObjDupParamsTo(obj);
@@ -43,5 +83,5 @@ namespace Ironbug.Grasshopper.Component
 
     }
 
-   
+
 }
